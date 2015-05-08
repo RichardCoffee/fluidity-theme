ad92b29581bf66ee8a502740c949385687eceb52
Richard Coffee <richard.coffee@gmail.com>
2015-05-07 23:03:46 -0500

    add browser_body_class function

--- includes/library.php
+++ includes/library.php
@@ -14,6 +14,24 @@ if (!function_exists('apply_clearfix')) {
   }
 }
 
+if (!function_exists('browser_body_class')) {
+  // http://www.smashingmagazine.com/2009/08/18/10-useful-wordpress-hook-hacks/
+  function browser_body_class($classes) {
+    global $is_lynx, $is_gecko, $is_IE, $is_opera, $is_NS4, $is_safari, $is_chrome, $is_iphone;
+    if     ($is_lynx)   $classes[] = 'lynx';
+    elseif ($is_gecko)  $classes[] = 'gecko';
+    elseif ($is_opera)  $classes[] = 'opera';
+    elseif ($is_NS4)    $classes[] = 'ns4';
+    elseif ($is_safari) $classes[] = 'safari';
+    elseif ($is_chrome) $classes[] = 'chrome';
+    elseif ($is_IE)     $classes[] = 'ie';
+    else                $classes[] = 'unknown';
+    if     ($is_iphone) $classes[] = 'iphone';
+    return $classes;
+  }
+  add_filter('body_class','browser_body_class');
+}
+
 // Limit length of title string
 if (!function_exists('browser_title')) {
   function browser_title($title,$sep) {
