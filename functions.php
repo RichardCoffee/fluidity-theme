88ce807754d439f01116b3ec86963ba47f2fefe7
Laptop 3 <richard.coffee@gmail.com>
2015-05-11 17:40:25 -0400

    add includes/options.php

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ require_once('includes/actions.php');
 require_once('includes/filters.php');
 require_once('includes/login.php');
 require_once('includes/misc.php');
+require_once('includes/options.php');
 require_once('includes/sidebars.php');
 require_once('includes/wp_bootstrap_navwalker.php');
 require_once('includes/third-party.php');
---
+++ includes/options.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ *  fluidity/includes/options.php
+ *
+ */
+
+if (!function_exists('tcc_design')) {
+  function tcc_design($option) {
+    static $data;
+    if (empty($data)) $data = get_option('tcc_options_design');
+    if (isset($data[$option])) return $data[$option];
+    return '';
+  }
+}
