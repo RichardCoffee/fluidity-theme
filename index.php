14c99b00275018832e27c6c1eba455b593d43aab
Laptop 3 <richard.coffee@gmail.com>
2015-05-12 22:31:30 -0400

    add col div

--- index.php
+++ index.php
@@ -8,28 +8,29 @@ get_header(); ?>
 
 <div class="<?php echo container_type('post'); ?>">
   <div class="row">
+    <div class="col-lg-12 col-md-12 col-sm-12 col-xs 12">
 
-    <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12"><?php
-      who_am_i(__FILE__);
-      if (have_posts()) {
-        while(have_posts()) {
-          the_post(); ?>
-          <h1><?php
-            the_title(); ?>
-          </h1>
-          <div class="inner-padding article"><?php
-            the_content(); ?>
-          </div><?php
-        }
-      } ?>
-    </div>
+      <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12"><?php
+        who_am_i(__FILE__);
+        if (have_posts()) {
+          while(have_posts()) {
+            the_post(); ?>
+            <h1><?php
+              the_title(); ?>
+            </h1>
+            <div class="inner-padding article"><?php
+              the_content(); ?>
+            </div><?php
+          }
+        } ?>
+      </div>
 
-    <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><?php
-      get_sidebar('standard'); ?>
-    </div>
+      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><?php
+        get_sidebar('standard'); ?>
+      </div>
 
+    </div><!-- .col-*-* -->
   </div><!-- .row -->
-
 </div><?php
 
 get_footer(); ?>
