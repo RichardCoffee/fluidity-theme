1dcaaa7fbcb2d9114b377172f6eb526119e1b447
Laptop 3 <richard.coffee@gmail.com>
2015-05-13 00:27:37 -0400

    more work on footer template

--- template_parts/footer.php
+++ template_parts/footer.php
@@ -11,25 +11,40 @@
   <div class="container ">
     <div class="row"><?php
       who_am_i(__FILE__); ?>
-      <div class="col-lg-3 col-md-3 hidden-sm hidden-xs ">
-        <a href="<?php echo home_url(); ?>/">
-          <img style="width:200px;  margin:0 auto;" class="img-responsive" src='<?php echo tcc_design('logo'); ?> ' alt="<?php bloginfo('name') ?>">
-        </a>
-        <h4 class="text-center"><?php echo get_bloginfo ('title');?> </h4> <!--- this needs to be editable option in theme options-->
+      <div class="col-lg-3 col-md-3 hidden-sm hidden-xs "><?php
+        $logo = tcc_design('logo');
+        if ($logo) { ?>
+          <a href="<?php echo home_url(); ?>/">
+            <img style="width:200px;  margin:0 auto;" class="img-responsive" src='<?php echo $logo; ?>' alt="<?php bloginfo('name'); ?>">
+          </a><?php
+        } ?>
+        <h4 class="text-center"><?php echo get_bloginfo('title'); ?></h4>
         <address class="text-center"><!-- FIXME: this needs to be editable option in theme options -->
-          189 Macon Street<br>
-          Brooklyn NY, 11216 Suit #2<br>
-          Office: 917 548 3997<br>
+          123 Main Street<br>
+          Van TX, 12345<br>
+          Office: 888 555 1212<br>
           Email: <a href="mailto:<?php echo get_option('admin_email'); ?>"><?php echo get_bloginfo ('title');?> </a>
         </address>
       </div>
       <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12"><?php
         get_sidebar('footer'); ?>
       </div>
-      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
-        <p class="text-center">Copyright <?php echo site_copyright_dates(); ?> <?php bloginfo('name'); ?>, All rights reserved.
-          <br>
-          <a class="text-center" href="/conditions/" >Terms & Conditions</a>   |   <a href="/privacy/" >Security & Privacy</a>
+      <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 "><?php
+        $format = _x('Copyright %1$s %2$s, All rights reserved.','First string will be a year, Second string is the site name','fluid-theme'); ?>
+        <p class="text-center"><?php
+          echo sprintf($format,site_copyright_dates(),bloginfo('name')); ?>
+          <br><?php
+          $foot_menu = array();
+          if (page_exists('conditions')) $foot_menu[] = array('conditions', __('Terms & Conditions','fluid-theme'));
+          if (page_exists('privacy'))    $foot_menu[] = array('privacy',    __('Privacy Policy','fluid-theme'));
+          if (page_exists('security'))   $foot_menu[] = array('security',   __('Security Policy','fluid-theme'));
+          $foot_menu = apply_filter('tcc_foot_menu',$foot_menu);
+          if ($foot_menu) {
+            $string = '';
+            foreach($foot_menu as $option) {
+              $string.= "<a href='/{$option[0]}/'> {$option[1]} </a> | "; }
+            echo substr($string,0,-3);
+          }  ?>
         </p>
       </div>
     </div><!-- .row -->
