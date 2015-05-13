2c6d3ff2df781aa3d213c6d80f823b5a73c25520
Laptop 3 <richard.coffee@gmail.com>
2015-05-12 23:27:21 -0400

    more work on 404

--- 404.php
+++ 404.php
@@ -26,15 +26,14 @@ if (WP_DEBUG) { ?>
 <div class="row text-center">
   <div class="col-md-12">
     <div class="article">
-      <a  href="<?php echo home_url() ; ?>/"><img src='<?php echo tcc_design('logo'); ?>'></a>
-      <h1  class="page-title"><?php _e( "Ooops.....Well this is somewhat embarrassing, isn't it?", 'fluid-theme' ); ?></h1>
+      <h1  class="page-title"><?php _e("Ooops.....Well this is somewhat embarrassing, isn't it?",'fluid-theme'); ?></h1>
       <div class="page-content">
-        <h2><?php _e( 'It seems as if the page you are looking for is not here', 'fluid-theme' ); ?></h2>
-        <p><?php _e( 'It looks like nothing was found at this location', 'fluid-theme' ); ?></p>
-        <a href="<?php echo home_url() ; ?>">Click here to go back home! </a>
+        <h2><?php _e('It seems as if the page you are looking for is not here','fluid-theme'); ?></h2>
+        <h3><?php _e('It looks like nothing was found at this location','fluid-theme'); ?></h3>
+        <a href="<?php echo home_url() ; ?>"><?php _e('Home Page','fluid-theme'); ?></a>
       </div><!-- .page-content -->
     </div><!-- .article -->
-  </div><!-- .col-md-12 .inner-padding -->
+  </div><!-- .col-md-12 -->
 </div><!-- .row --><?php
 
 get_footer(); ?>
