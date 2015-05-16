c9cf9fd6462e2ec9d7cfdabe1e377e629376a369
Laptop 3 <richard.coffee@gmail.com>
2015-05-16 17:45:22 -0400

    bugfix

--- sidebar.php
+++ sidebar.php
@@ -8,18 +8,18 @@ who_am_i();
 
 $loaded = false;
 if (is_front_page()) {
-  echo "<p>front page</p>";
+#  echo "<p>front page</p>";
   $loaded = load_sidebar(array('front','standard'));
 } else {
-  echo "<p>another page</p>";
+#  echo "<p>another page</p>";
   $loaded = load_sidebar(array(get_sidebar_parameter(),'standard'));
 }
 if (!$loaded) {
-  echo "<p>looking for post type</p>";
+#  echo "<p>looking for post type</p>";
   global $wp_query;
   $post_type = $wp_query->get('post_type');
   if ($post_type) {
-    echo "<p>looking for post type $post_type</p>";
+#    echo "<p>looking for post type $post_type</p>";
     $loaded = load_sidebar(array($post_type,$post_type.'_sidebar','standard'));
   }
 }
@@ -35,12 +35,12 @@ function get_sidebar_parameter() {
 }
 
 function load_sidebar($sidebars) {
-  echo "<pre>"; print_r($sidebars); echo "</pre>";
+#  echo "<pre>"; print_r($sidebars); echo "</pre>";
   foreach($sidebars as $sidebar) {
     if (is_active_sidebar($sidebar)) {
       if (dynamic_sidebar($sidebar)) {
         return true;
-      } else { echo "<p>$sidebar non-dynamic</p>"; }
+      } else { /*echo "<p>$sidebar non-dynamic</p>";*/ }
     } else { /*echo "<p>$sidebar not active</p>";*/ }
   }
   return false;
