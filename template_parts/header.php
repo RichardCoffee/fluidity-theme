a8b885244b3cb2fabc8a83940c82f43760d0b278
Laptop 3 <richard.coffee@gmail.com>
2015-05-17 01:55:23 -0400

    bugfix

--- template_parts/header.php
+++ template_parts/header.php
@@ -9,7 +9,7 @@ $color_scheme = tcc_color_scheme(); ?>
 
 <div id="fluid-header" class="<?php echo container_type('fluid-header'); ?>">
 
-  <div id="header-topmenu" class="navbar-<?php echo $color_scheme; ?>"><?php
+  <div id="header-topmenu" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
     do_action('tcc_header_top_menubar'); ?>
   </div>
 
@@ -17,7 +17,7 @@ $color_scheme = tcc_color_scheme(); ?>
     do_action('tcc_header_body_content'); ?>
   </div>
 
-  <div id="header-menubar" class="navbar-<?php echo $color_scheme; ?>"><?php
+  <div id="header-menubar" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
     do_action('tcc_header_bottom_menubar'); ?>
   </div>
 
