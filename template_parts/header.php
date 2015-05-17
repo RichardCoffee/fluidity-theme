db2dff5c32c49504d5c354464c9e944268969193
Laptop 3 <richard.coffee@gmail.com>
2015-05-17 02:30:05 -0400

    add has_action calls

--- template_parts/header.php
+++ template_parts/header.php
@@ -7,18 +7,24 @@
 
 $color_scheme = tcc_color_scheme(); ?>
 
-<div id="fluid-header" class="<?php echo container_type('fluid-header'); ?>">
+<div id="fluid-header" class="<?php echo container_type('fluid-header'); ?>"><?php
 
-  <div id="header-topmenu" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
-    do_action('tcc_header_top_menubar'); ?>
-  </div>
+  if (has_action('tcc_header_top_menubar')) { ?>
+    <div id="header-topmenu" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
+      do_action('tcc_header_top_menubar'); ?>
+    </div><?php
+  }
 
-  <div id="header-body" class="row"><?php
-    do_action('tcc_header_body_content'); ?>
-  </div>
+  if (has_action('tcc_header_body_content')) { ?>
+    <div id="header-body" class="row"><?php
+      do_action('tcc_header_body_content'); ?>
+    </div><?php
+  }
 
-  <div id="header-menubar" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
-    do_action('tcc_header_bottom_menubar'); ?>
-  </div>
+  if (has_action('tcc_header_bottom_menubar')) { ?>
+    <div id="header-menubar" class="navbar navbar-<?php echo $color_scheme; ?>"><?php
+      do_action('tcc_header_bottom_menubar'); ?>
+    </div><?php
+  } ?>
 
 </div>
