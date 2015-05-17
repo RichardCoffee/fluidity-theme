905488fc33bde87d0c233ce9b57000a1b4dca597
Laptop 3 <richard.coffee@gmail.com>
2015-05-17 01:19:32 -0400

    move class to id

--- template_parts/header.php
+++ template_parts/header.php
@@ -6,17 +6,18 @@
  */
 
 ?>
+
 <div id="fluid-header" class="<?php echo container_type('fluid-header'); ?>">
 
-  <div class="row header-topmenu"><?php
+  <div id="header-topmenu" class="row"><?php
     do_action('tcc_header_top_menubar'); ?>
   </div>
 
-  <div class="row header-body"><?php
+  <div id="header-body" class="row"><?php
     do_action('tcc_header_body_content'); ?>
   </div>
 
-  <div class="row header-menubar"><?php
+  <div id="header-menubar" class="row"><?php
     do_action('tcc_header_bottom_menubar'); ?>
   </div>
 
