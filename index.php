10fc57d070440ad06241ed58d19ae7c8fd38361d
Laptop 3 <richard.coffee@gmail.com>
2015-05-16 20:18:04 -0400

    testing

--- index.php
+++ index.php
@@ -10,28 +10,26 @@ get_header(); ?>
   <div class="row">
     <div class="col-lg-12 col-md-12 col-sm-12 col-xs 12">
 
-      <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12"><?php
-        who_am_i(__FILE__);
-        if (have_posts()) {
-          while(have_posts()) {
-            the_post();
-            $format  = __('Permanent Link to %s','tcc-fluid');
-            $tooltip = sprintf($format,get_the_title()); ?>
-            <h1 class="text-center">
-              <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo $tooltip; ?>"><?php echo fluid_title(20); ?></a>
-            </h1>
-            <h3 class="text-center"><?php echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),get_the_author()); ?></h3>
-            <div class="article"><?php
-              the_content(); ?>
-            </div><?php
-            comments_template();
-          }
-        } ?>
-      </div>
-
-      <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><?php
+      <div class="col-lg-4 col-md-4 hidden-sm hidden-xs pull-right"><?php
         get_sidebar('standard'); ?>
-      </div>
+      </div><?php
+
+      who_am_i(__FILE__);
+      if (have_posts()) {
+        while(have_posts()) {
+          the_post();
+          $format  = __('Permanent Link to %s','tcc-fluid');
+          $tooltip = sprintf($format,get_the_title()); ?>
+          <h1 class="text-center">
+            <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php echo $tooltip; ?>"><?php echo fluid_title(20); ?></a>
+          </h1>
+          <h3 class="text-center"><?php echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),get_the_author()); ?></h3>
+          <div class="article"><?php
+            the_content(); ?>
+          </div><?php
+          comments_template();
+        }
+      } ?>
 
     </div><!-- .col-*-* -->
   </div><!-- .row -->
