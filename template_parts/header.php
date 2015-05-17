9db25bfc5ebf7212fb6d0a2aca90c82d9c7b5534
Laptop 3 <richard.coffee@gmail.com>
2015-05-17 01:54:15 -0400

    add color scheme to class

--- template_parts/header.php
+++ template_parts/header.php
@@ -5,11 +5,11 @@
  *
  */
 
-?>
+$color_scheme = tcc_color_scheme(); ?>
 
 <div id="fluid-header" class="<?php echo container_type('fluid-header'); ?>">
 
-  <div id="header-topmenu" class="navbar"><?php
+  <div id="header-topmenu" class="navbar-<?php echo $color_scheme; ?>"><?php
     do_action('tcc_header_top_menubar'); ?>
   </div>
 
@@ -17,7 +17,7 @@
     do_action('tcc_header_body_content'); ?>
   </div>
 
-  <div id="header-menubar" class="navbar"><?php
+  <div id="header-menubar" class="navbar-<?php echo $color_scheme; ?>"><?php
     do_action('tcc_header_bottom_menubar'); ?>
   </div>
 
