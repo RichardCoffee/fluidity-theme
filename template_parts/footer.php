6a915b65994575fb28660b0033fa08c002955621
Laptop 3 <richard.coffee@gmail.com>
2015-05-18 18:17:27 -0400

    more microdata

--- template_parts/footer.php
+++ template_parts/footer.php
@@ -14,8 +14,8 @@ global $micro; ?>
   <div class="col-lg-3 col-md-3 hidden-sm hidden-xs" itemprop='author' itemscope itemtype='http://schema.org/Organization'><?php
     $logo = tcc_design('logo');
     if ($logo) { ?>
-      <a href="<?php bloginfo('url'); ?>/" itemprop="logo">
-        <img style="width:200px;  margin:0 auto;" class="img-responsive" src='<?php echo $logo; ?>' alt="<?php bloginfo('name'); ?>">
+      <a href="<?php bloginfo('url'); ?>/">
+        <img itemprop="logo" style="width:200px;  margin:0 auto;" class="img-responsive" src='<?php echo $logo; ?>' alt="<?php bloginfo('name'); ?>">
       </a><?php
     } ?>
     <h4 class="text-center" itemprop="name"><?php bloginfo('title'); ?></h4>
@@ -27,7 +27,7 @@ global $micro; ?>
       Email: <a href="mailto:<?php echo get_option('admin_email'); ?>"><?php bloginfo ('title');?> </a>
     </address>
   </div>
-  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12"><?php
+  <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12" <?php $micro->SideBar(); ?>><?php
     get_sidebar('footer'); ?>
   </div>
   <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
@@ -36,15 +36,16 @@ global $micro; ?>
       echo sprintf($format,site_copyright_dates(),$micro->get_bloginfo('name')); ?>
       <br><?php
       $foot_menu = array();
+      if (page_exists('terms'))      $foot_menu[] = array('terms',      __('Terms & Conditions','tcc-fluid'));
       if (page_exists('conditions')) $foot_menu[] = array('conditions', __('Terms & Conditions','tcc-fluid'));
       if (page_exists('privacy'))    $foot_menu[] = array('privacy',    __('Privacy Policy',    'tcc-fluid'));
       if (page_exists('security'))   $foot_menu[] = array('security',   __('Security Policy',   'tcc-fluid'));
       $foot_menu = apply_filters('tcc_bottom_menu',$foot_menu);
       if ($foot_menu) {
-        $string = '';
+        $string = '<span '.$micro->Navigation().'>';
         foreach($foot_menu as $option) {
           $string.= "<a href='/{$option[0]}/'> {$option[1]} </a> | "; }
-        echo substr($string,0,-3);
+        echo substr($string,0,-3).'</span>';
       }  ?>
     </p>
   </div>
