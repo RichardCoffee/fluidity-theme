9401aac7bcc9e27f9dcce44ab4d39a66aeafa7bb
Laptop 3 <richard.coffee@gmail.com>
2015-05-20 16:34:24 -0400

    add searchform.php

---
+++ searchform.php
@@ -0,0 +1,9 @@
+
+<form role="search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>" role="form">
+    <div class="input-group">
+        <input type="text" value="" name="s" id="s" class="form-control" placeholder="<?php _e('Search','creatom'); ?>" />
+        <span class="input-group-btn">
+            <button class="btn btn-<?php echo tcc_color_scheme('searchform'); ?>" type="submit"><i class="fa fa-search"></i></button>
+        </span>
+    </div>
+</form>
