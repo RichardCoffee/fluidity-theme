d2c48d56e318e75bf5e91832e78cf66bf3dceed3
Laptop 3 <richard.coffee@gmail.com>
2015-05-20 22:29:54 -0400

    add title link

--- template_parts/content.php
+++ template_parts/content.php
@@ -6,22 +6,22 @@
  */
 
 global $micro;
-
+$href = get_the_permalink();
 who_am_i(); ?>
 
 <div id="post-<?php the_ID(); ?> " <?php post_class(); ?> <?php $micro->BlogPosting(); ?>>
 
-  <h4 class='text-center'><?php
-    the_title(); ?>
+  <h4 class="text-center">
+    <a href="<?php echo $href; ?>" itemprop="url"><?php the_title(); ?></a>
   </h4><?php
 
   if (has_post_thumbnail()) { ?>
-    <a href='<?php echo get_the_permalink(); ?>'><?php
+    <a href="<?php echo $href; ?>" itemprop="url"><?php
       the_post_thumbnail('', array('class' => 'img-responsive listpost-img')); ?>
     </a><?php
   } ?>
 
-  <div itemprop="text"><?php
+  <div itemprop="description"><?php
     the_excerpt(); ?>
   </div>
 
