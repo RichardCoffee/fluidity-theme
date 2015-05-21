385e680397b0543f8ef6829cb4d4c428c5a9865a
Laptop 3 <richard.coffee@gmail.com>
2015-05-20 20:07:36 -0400

    add content to class

--- template_parts/content.php
+++ template_parts/content.php
@@ -1,22 +1,22 @@
 <?php
 
 /*
- *  template_parts.content.php
+ *  tcc-fluidity/template_parts/content.php
  *
  */
 
 global $micro;
-$href = get_the_permalink();
+
 who_am_i(); ?>
 
-<div id="post-<?php the_ID(); ?> " <?php post_class(); ?> <?php $micro->BlogPosting(); ?>>
+<article id="post-<?php the_ID(); ?> " <?php post_class('content'); ?> <?php $micro->BlogPosting(); ?>>
 
   <h4 class='text-center'><?php
     the_title(); ?>
   </h4><?php
 
   if (has_post_thumbnail()) { ?>
-    <a href='<?php echo $href; ?>'><?php
+    <a href='<?php echo get_the_permalink(); ?>'><?php
       the_post_thumbnail('', array('class' => 'img-responsive listpost-img')); ?>
     </a><?php
   } ?>
@@ -25,4 +25,4 @@ who_am_i(); ?>
     the_excerpt(); ?>
   </div>
 
-</div>
+</article>
