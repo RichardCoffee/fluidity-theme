34c28cad478f362e8b47d18f9d81ce44a2e550ac
Richard Coffee <richard.coffee@gmail.com>
2015-05-22 02:45:15 -0500

    bugfix

--- archive.php
+++ archive.php
@@ -11,7 +11,7 @@ $micro = $fluid->microdata;
 
 get_header(); ?>
 
-<div class="<?php echo container_type($fluid->page); ?>" <?php if (is_search()) $micro->SearchResultsPage(); ?>>
+<div class="<?php echo container_type($fluid->sidebar_name); ?>" <?php if (is_search()) $micro->SearchResultsPage(); ?>>
   <div class="row"><?php
     who_am_i();
 
