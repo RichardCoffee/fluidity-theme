30cd08ff5530181497b04f871242dec550476610
Richard Coffee <richard.coffee@gmail.com>
2015-05-25 00:59:48 -0500

    change color scheme

--- includes/options.php
+++ includes/options.php
@@ -41,11 +41,13 @@ if (!function_exists('tcc_color_scheme')) {
  *     red: danger
  *   white: default
  */
-    return 'success';
+#    $colors - array('primary','success','success-inverse','info','info-inverse','warning','warning-inverse','danger','danger-inverse');
+#    $color  = rand(0,count($colors)-1);
+#    return $color;
 #    return 'success-inverse';
 #    return 'primary';
 #    return 'info';
 #    return 'warning';
-#    return 'danger';
+    return 'danger';
   }
 }
