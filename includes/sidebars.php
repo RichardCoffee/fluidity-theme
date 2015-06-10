9035754c441778fea24e51d12f824046f1fb2411
Richard Coffee <richard.coffee@gmail.com>
2015-06-10 03:59:27 -0500

    update

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -2,44 +2,44 @@
 
 //This sets up the widget area sidebars
 
-if (!function_exists('fluidity_register__sidebars')) {
+if (!function_exists('fluidity_register_sidebars')) {
   function fluidity_register_sidebars() {
 
-    $color    = tcc_color_scheme('sidebars');
-    $b_widget = "<div class='panel panel-$color'>";
-    $b_title  = "<div class='panel-heading'><h3 class='panel-title text-center'><b>";
-    $a_title  = "</b></h3></div><div class='panel-body'>";
-    $a_widget = "</div></div>";
+    $color = tcc_color_scheme('sidebars');
+    $before_widget = "<div class='panel panel-$color'>";
+    $before_title  = "<div class='panel-heading'><h3 class='panel-title text-center'><b>";
+    $after_title   = "</b></h3></div><div class='panel-body'>";
+    $after_widget  = "</div></div>";
 
     $sidebars = array();
 
     $sidebars[] = array('name'          => __('Standard Page Sidebar','tcc-fluid'),
                         'id'            => 'standard',
-                        'before_widget' => $b_widget,
-                        'before_title'  => $b_title,
-                        'after_title'   => $a_title,
-                        'after_widget'  => $a_widget);
+                        'before_widget' => $before_widget,
+                        'before_title'  => $before_title,
+                        'after_title'   => $after_title,
+                        'after_widget'  => $after_widget);
 
     $sidebars[] = array('name'          => __('Front Page Sidebar','tcc-fluid'),
                         'id'            => 'front',
-                        'before_widget' => $b_widget,
-                        'before_title'  => $b_title,
-                        'after_title'   => $a_title,
-                        'after_widget'  => $a_widget);
+                        'before_widget' => $before_widget,
+                        'before_title'  => $before_title,
+                        'after_title'   => $after_title,
+                        'after_widget'  => $after_widget);
 
     $sidebars[] = array('name'          => __('Horizontal Sidebar (3 col)','tcc-fluid'),
                         'id'            => 'three_column',
-                        'before_widget' => "<div class='col-lg-4 col-md-4 col-sm-12 col-xs-12'>$b_widget",
-                        'before_title'  => $b_title,
-                        'after_title'   => $a_title,
-                        'after_widget'  => "$a_widget</div>"); //*/
+                        'before_widget' => "<div class='col-lg-4 col-md-4 col-sm-12 col-xs-12'>$before_widget",
+                        'before_title'  => $before_title,
+                        'after_title'   => $after_title,
+                        'after_widget'  => "$after_widget</div>"); //*/
 
-    $sidebars[] = array('name'          => __('Footer Widget Area (3 col)','tcc-fluid'),
+    $sidebars[] = array('name'          => __('Footer Widget Area (4 col)','tcc-fluid'),
                         'id'            => 'footer',
-                        'before_widget' => "<div class='col-lg-4 col-md-4 col-sm-12 col-xs-12'>$b_widget",
-                        'before_title'  => $b_title,
-                        'after_title'   => $a_title,
-                        'after_widget'  => "$a_widget</div>");
+                        'before_widget' => "<div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>$before_widget",
+                        'before_title'  => $before_title,
+                        'after_title'   => $after_title,
+                        'after_widget'  => "$after_widget</div>");
 
     $sidebars = apply_filters('tcc_register_sidebars',$sidebars);
 
