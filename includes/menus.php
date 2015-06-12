0757439725bde4b014e1305c6fd7bf2ba962934d
Richard Coffee <richard.coffee@gmail.com>
2015-06-12 03:26:27 -0500

    tracking

--- includes/menus.php
+++ includes/menus.php
@@ -10,6 +10,7 @@ if (!function_exists('tcc_register_nav_menu')) {
     register_nav_menu('primary',__('Primary Menu','tcc-fluid'));
     register_nav_menu('header',__('Header Menu','tcc-fluid'));
     register_nav_menu('footer',__('Footer Menu','tcc-fluid'));
+    log_entry("theme name: ".wp_get_theme());
   }
   add_action('after_setup_theme','tcc_register_nav_menu');
 }
