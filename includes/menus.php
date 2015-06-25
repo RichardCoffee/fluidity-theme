48f91562741b7890eb78370fa34f1079a94c601b
Richard Coffee <richard.coffee@gmail.com>
2015-06-25 02:23:02 -0500

    testing nav menu items

--- includes/menus.php
+++ includes/menus.php
@@ -3,6 +3,9 @@
 /*
  * includes/menus.php
  *
+ * Sources: http://codex.wordpress.org/Navigation_Menus
+ *          http://catapultdesign.co.uk/programmatically-create-menu-in-wordpress/
+ *
  */
 
 if (!function_exists('tcc_register_nav_menu')) {
@@ -10,70 +13,31 @@ if (!function_exists('tcc_register_nav_menu')) {
     $menus = array('primary' => __('Primary Menu','tcc-fluid'),
                    'header'  => __('Header Menu','tcc-fluid'),
                    'footer'  => __('Footer Menu','tcc-fluid'));
+    $items = array('primary' => array(array('menu-item-title'  => __('Home','tcc-fluid'),
+                                            'menu-item-url'    => home_url('/'),
+                                            'menu-item-status' => 'publish')),
+                   'header'  => array(),
+                   'footer'  => array());
     register_nav_menus($menus);
+    $theme_slug = strtolower(str_replace(' ','_',wp_get_theme()));
+    $theme_mods = get_option('theme_mods_'.$theme_slug);
+    $updated    = false;
+    foreach($menus as $key=>$title) {
+      if (count($items[$key])==0) continue;
+      if (!is_nav_menu($key)) {
+        $menu_id = wp_create_nav_menu($title);
+        foreach($items[$key] as $item) {
+          wp_update_nav_menu_item($menu_id,0,$item);
+        }
+        if (!isset($theme_mods['nav_menu_locations']))
+          $theme_mods['nav_menu_locations'] = array();
+        if (!isset($theme_mods['nav_menu_locations'][$key])) {
+          $theme_mods['nav_menu_locations'][$key] = $menu_id;
+          $updated = true;
+        }
+      }
+    }
+    if ($updated) update_option('theme_mods_'.$theme_slug,$theme_mods);
   }
   add_action('init','tcc_register_nav_menu');
 }
-
-/*if (!function_exists('tcc_register_nav_menu')) {
-  function tcc_register_nav_menu() {
-    $menus = array(array('slug'  => 'primary',
-                         'title' => __('Primary Menu','tcc-fluid'),
-                         'items' => array()),
-                   array('slug'  => 'header',
-                         'title' => __('Header Menu','tcc-fluid'),
-                         'items' => array()),
-                   array('slug'  => 'footer',
-                         'title' => __('Footer Menu','tcc-fluid'),
-                         'items' => array()));
-    foreach($menus as $menu) {
-      tcc_check_nav_menu($menu);
-    }
-$mods = get_theme_mods();
-log_entry('theme mods',$mods);
-  }
-  add_action('after_setup_theme','tcc_register_nav_menu');
-}
-
-if (!function_exists('tcc_check_nav_menu')) {
-  function tcc_check_nav_menu($menu) {
-    if (!is_nav_menu($menu['slug'])) {
-log_entry("menu not found at {$menu['slug']}");
-    }
-  }
-}
-
-/*function catapult_check_menus() {
-
- // If the Primary Menu doesn't exist, let's create it
- if ( ! is_nav_menu( 'primary-menu' ) ) {
- 
-  // Create menu
-  $menu_id = wp_create_nav_menu ( 'Primary Menu' );
- 
-  // Add menu items
-  wp_update_nav_menu_item ( $menu_id, 0, array(
-   'menu-item-title' => __('Home'),
-   'menu-item-url' => home_url( '/' ), 
-   'menu-item-status' => 'publish' ) );
-
-  wp_update_nav_menu_item ( $menu_id, 0, array (
-   'menu-item-title' => __('Shop'),
-   'menu-item-url' => home_url( '/shop/' ), 
-   'menu-item-status' => 'publish' ) );
- 
-  // Lower case theme_name
-  $theme = strtolower ( str_replace ( ' ', '_', wp_get_theme() ) );
- 
-  // Get the theme options
-  $theme_mods = get_option ( 'theme_mods_' . $theme );
- 
-  // Set the location of the primary menu
-  $theme_mods['nav_menu_locations'] = array ( 'primary-menu' => $menu_id );
- 
-  // Update the theme options
-  update_option ( 'theme_mods_' . $theme, $theme_mods );
- 
- }
-
-}//*/
