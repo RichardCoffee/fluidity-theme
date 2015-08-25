1d7f2acda76111e98e16c8794d86c2710287256b
Laptop 3 <richard.coffee@gmail.com>
2015-08-25 02:19:47 -0400

    change the content to the excerpt

--- index.php
+++ index.php
@@ -32,7 +32,7 @@ get_header(); ?>
               echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),$micro->get_the_author()); ?>
             </h3>
             <div class="article" itemprop="articleBody"><?php
-              the_content(); ?>
+              the_excerpt(); ?>
             </div><?php
             comments_template();
           }
