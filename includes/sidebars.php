03992f26ee2dd3b43c62b6b2b69eb18c1e290a38
Laptop 3 <richard.coffee@gmail.com>
2016-01-30 23:35:20 -0500

    tracking

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -4,7 +4,9 @@
 
 if (!function_exists('fluidity_register_sidebars')) {
   function fluidity_register_sidebars() {
+echo "<pre>";
 print_r(debug_backtrace());
+echo "</pre>";
     $color = tcc_color_scheme('sidebars');
     $before_widget = "<div class='panel panel-$color'>";
     $before_title  = "<div class='panel-heading'><h3 class='panel-title text-center'><b>";
