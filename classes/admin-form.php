646b22c0a383efd272cb75dc6b3197cce7b5e1f3
Laptop 3 <richard.coffee@gmail.com>
2016-01-31 09:06:11 -0500

    change group name

--- classes/admin-form.php
+++ classes/admin-form.php
@@ -105,7 +105,7 @@ global $new_whitelist_options, $wp_settings_sections,$wp_settings_fields;
       $validate = (isset($section['validate'])) ? $section['validate'] : $validater;
       $describe = (isset($section['describe'])) ? $section['describe'] : 'description';
       $current  = (isset($this->form[$key]['option'])) ? $this->form[$key]['option'] : $this->prefix.$key;
-      register_setting($current,$current,array($this,$validate));
+      register_setting($this->slug,$current,array($this,$validate));
 #log_entry("register    group: $current");
 #log_entry("register   option: $current");
 #log_entry("register callback: $validate");
