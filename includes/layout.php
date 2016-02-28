32398c088ec050a00ab5ed7f62b477e37629839b
Laptop 3 <richard.coffee@gmail.com>
2016-02-27 22:20:06 -0500

    remove parameter from fluid_layout

--- includes/layout.php
+++ includes/layout.php
@@ -6,21 +6,20 @@
  */
 
 if (!function_exists('fluid_layout')) {
-  function fluid_layout($sidebar='standard') {
+  function fluid_layout() {
     $layout = new stdClass();
-    $layout->sidebar    = $sidebar;
-    $layout->sb_locate  = fluidity_sidebar_side($sidebar);
+    $layout->sb_locate  = 'left';
     $layout->sb_class   = 'col-lg-4  col-md-4  col-sm-12 col-xs-12';
-    $layout->main_class = ($layout->sb_locate=='none') ? 'col-lg-12 col-md-12 col-sm-12 col-xs-12' : 'col-lg-8  col-md-8  col-sm-12 col-sm-12';
+#    $layout->main_class = ($layout->sb_locate=='none') ? 'col-lg-12 col-md-12 col-sm-12 col-xs-12' : 'col-lg-8  col-md-8  col-sm-12 col-sm-12';
     return apply_filters('fluid_layout',$layout);
   }
 }
 
 if (!function_exists('sidebar_layout')) {
   function sidebar_layout($sidebar='standard') {
-    $layout = fluid_layout($sidebar);
+    $layout = fluid_layout();
     if ($layout->sb_locate!=='none') {
-      $sidebar_class.= $layout->sb_class.(($layout->sb_locate=='right') ? ' pull-right' : ''); ?>
+      $sidebar_class = $layout->sb_class.(($layout->sb_locate=='right') ? ' pull-right' : ''); ?>
       <aside class="<? echo $sidebar_class; ?>" <?php $micro->WPSideBar(); ?>><?php
         fluidity_get_sidebar($sidebar); ?>
       </aside><?php
