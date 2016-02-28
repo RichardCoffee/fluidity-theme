840690e638c0f08b37ee6d18607ead01e661357f
Laptop 3 <richard.coffee@gmail.com>
2016-02-27 22:47:14 -0500

    add padbott class

--- page.php
+++ page.php
@@ -11,27 +11,31 @@ $micro  = microdata();
 $layout = fluid_layout(); ?>
 
 <div class="<?php echo container_type('page'); ?>">
-  <div class="row"><?php
+  <div class="row">
+    <div class="col-xs-12"><?php
 
-    sidebar_layout('standard'); ?>
+      sidebar_layout('standard'); ?>
 
-    <div class="<?php echo $col_primary; ?>"><?php
-      who_am_i();
-      if (have_posts()) {
-        while (have_posts()) {
-          the_post(); ?>
-          <h1 class="text-center"><?php
-            echo fluid_title(); // inserts a space
-            edit_post_link(__('{Edit}','tcc-fluid'), 'tcc-fluid'); ?>
-          </h1>
-          <h3 class="text-center"><?php
-            echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),$micro->get_the_author(true)); ?>
-          </h3>
-          <div class="article"><?php
-            the_content(); ?>
-          </div><?php
-        }
-      } ?>
+      <div class="<?php echo $col_primary; ?>"><?php
+        who_am_i();
+        if (have_posts()) {
+          while (have_posts()) {
+            the_post(); ?>
+            <h1 class="text-center"><?php
+              echo fluid_title(); // inserts a space
+              edit_post_link(__('{Edit}','tcc-fluid'), ' '); ?>
+            </h1>
+            <h3 class="text-center"><?php
+              echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),$micro->get_the_author(true)); ?>
+            </h3>
+            <div class="article"><?php
+              the_content(); ?>
+            </div><?php
+            if (have_posts()) echo "<hr class='padbott'>";
+          }
+        } ?>
+      </div>
+
     </div>
   </div>
 </div><?php
