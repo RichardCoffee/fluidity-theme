5690c5c7317710027ed89c900daf120502086565
Laptop 3 <richard.coffee@gmail.com>
2016-02-29 02:31:27 -0500

    remove row div, change link divs to spans

--- single.php
+++ single.php
@@ -47,14 +47,12 @@ $micro = microdata(); ?>
                 comments_popup_link(__('No Comments','tcc-fluid'),__('1 Comment','tcc-fluid'),_x('% Comments',"This string for multiple comments,'%' will be replaced with a number",'tcc-fluid')); ?>
               </p>
 
-              <div class='row'>
-                <div class='pull-left'><?php
-                  previous_post_link(); ?>
-                </div>
-                <div class='pull-right'><?php
-                  next_post_link(); ?>
-                </div>
-              </div><?php
+              <span class='pull-left'><?php
+                previous_post_link(); ?>
+              </span>
+              <span class='pull-right'><?php
+                next_post_link(); ?>
+              </span><?php
 
               comments_template(); ?>
 
