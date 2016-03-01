8671a97dab7a436cf596238e7366842c890756b2
Laptop 3 <richard.coffee@gmail.com>
2016-03-01 04:23:59 -0500

    testing

--- includes/header.php
+++ includes/header.php
@@ -114,7 +114,7 @@ if (!function_exists('fluidity_header_logo')) {
 
 if ((!function_exists('fluidity_main_menubar')) && (file_exists(get_template_directory().'/template-parts/menu.php'))) {
   function fluidity_main_menubar() {
-#echo "Color Scheme: ".tcc_color_scheme();
+echo "Color Scheme: ".tcc_color_scheme();
 //    if ($menu=tcc_layout('menu')) {
 #log_entry("layout menu: $menu");
 #assert( "locate_template( array('template-parts/menu-$menu.php', 'template-parts/menu.php'), false, false )" );
