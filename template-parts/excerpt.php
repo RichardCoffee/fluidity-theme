7479d666ec0116041d6d2256dcaa7acca8ba3496
Laptop 3 <richard.coffee@gmail.com>
2016-03-02 00:10:46 -0500

    remove esc_attr call

--- template-parts/excerpt.php
+++ template-parts/excerpt.php
@@ -15,7 +15,7 @@ who_am_i(); ?>
     <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php esc_attr_e($tooltip); ?>"><?php echo fluid_title(40); ?></a>
   </h1>
   <h3 class="text-center"><?php
-    esc_attr_e(sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),$micro->get_the_author())); ?>
+    echo sprintf(__('Posted on %1$s by %2$s','tcc-fluid'),get_the_date(),$micro->get_the_author()); ?>
   </h3>
   <div class="article" itemprop="description"><?php
     the_excerpt(); ?>
