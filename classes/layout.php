c722f0dfad4c2432e67b0212f8dd5237d95e85be
Laptop 3 <richard.coffee@gmail.com>
2016-03-23 16:09:03 -0400

    bugfix - maybe

--- classes/layout.php
+++ classes/layout.php
@@ -81,7 +81,8 @@ class Theme_Layout_Options {
     $layout['site']   = array('label'   => __('Site',        'tcc-fluid'),
                               'text'    => __('Coming Soon!','tcc-fluid'),
                               'render'  => 'display'); //*/
-    $layout = apply_filters('tcc_'.$this->base.'_options_layout',$layout);
+    $filter = 'tcc_'.$this->base.'_options_layout';
+    $layout = apply_filters($filter,$layout);
     return $layout;
   }
 
