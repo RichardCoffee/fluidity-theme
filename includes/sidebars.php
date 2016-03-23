0f15cc01e94ddefdc0c227d52b03ea1202118029
Laptop 3 <richard.coffee@gmail.com>
2016-03-23 11:59:27 -0400

    remove widget title html from footer sidebar

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -36,9 +36,9 @@ if (!function_exists('fluidity_register_sidebars')) {
     #  Footer sidebar
     $sidebars[] = array('name'          => __('Footer Widget Area (4 col)','tcc-fluid'),
                         'id'            => 'footer',
-                        'before_widget' => "<div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>$before_widget",
-                        'before_title'  => $before_title,
-                        'after_title'   => $after_title,
+                        'before_widget' => "<div class='col-lg-3 col-md-3 col-sm-6 col-xs-12'>$before_widget<div class='panel-body'>",
+                        'before_title'  => '',
+                        'after_title'   => '',
                         'after_widget'  => "$after_widget</div>");
     #  apply filters
     $sidebars = apply_filters('tcc_register_sidebars',$sidebars);
