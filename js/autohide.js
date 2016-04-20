1611217cc076b77cdff95f9978189f24f83691b7
Laptop 3 <richard.coffee@gmail.com>
2016-04-20 11:27:13 -0400

    more work on fixed header

--- js/autohide.js
+++ js/autohide.js
@@ -1,12 +1,17 @@
 
 //  https://medium.com/@mariusc23/hide-header-on-scroll-down-show-on-scroll-up-67bbaae9a78c
 //  http://stackoverflow.com/questions/24765185/hide-fixed-header-on-scroll-down-show-on-scroll-up-and-hover
+//  http://www.jqueryscript.net/other/Smooth-Auto-Hide-Header-Navigation-with-jQuery-CSS3.html
 
 var autohide = { did:   false, // did a scroll occur?
                  last:  0,     // last scroll position
                  delta: 5,     // action threshold
                  bar:   jQuery('#fluid-header').outerHeight() // height of header
                }
+
+jQuery(document).ready(function() {
+  jQuery('#fluid-header').next().css({padding-top:autohide.bar});
+}
 
 jQuery(window).scroll(function(event){
   autohide.did = true;
@@ -21,20 +26,19 @@ setInterval(function() {
 
 function hasScrolled() {
   var st = jQuery(this).scrollTop();
-  // Make sure they scroll more than delta
+console.log('st: '+st);
   if(Math.abs(autohide.last - st) <= delta) return;
-  // If they scrolled down and are past the navbar, add class .nav-up.
-  // This is necessary so you never see what is "behind" the navbar.
+console.log('last: '+autohide.last+'   bar: '+autohide.bar);
   if (st > autohide.last && st > autohide.bar){
-    // Scroll Down
-    jQuery('#fluid-header').hide('slow') // .addClass('nav-hide')
-      .hover(function() { // css:  #fluid-header { border-bottom: 1em solid transparent; }
-        jQuery("#fluid-header").show('slow'); // .removeClass('nav-hide');
+console.log('scroll down');
+    jQuery('#fluid-header').css({top:(-autohide.bar)+'px'}); // .hide('slow') // .addClass('nav-hide')
+      .hover(function() {
+        jQuery("#fluid-header").css({top:'0px'}); // .show('slow'); // .removeClass('nav-hide');
       });
   } else {
-    // Scroll Up
+console.log('scroll up');
     if(st + jQuery(window).height() < jQuery(document).height()) {
-      jQuery('#fluid-header').show('slow');  // .removeClass('nav-hide');
+      jQuery('#fluid-header').css({top:'0px'}); // .show('slow'); // .removeClass('nav-hide');
     }
   }
   autohide.last = st;
