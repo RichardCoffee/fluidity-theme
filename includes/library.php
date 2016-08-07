80cf892120ab4bba6f434bd0f00015e5faef9e4c
Laptop 3 <richard.coffee@gmail.com>
2016-08-07 11:19:45 -0400

    redo logging logic

--- includes/library.php
+++ includes/library.php
@@ -286,13 +286,24 @@ if (!function_exists('list_filter_hooks')) {
 if (!function_exists('log_entry')) {
   function log_entry() {
     if (WP_DEBUG) {
-      args  = func_get_args();
+      $args  = func_get_args(); /*
       $depth = 1;
       if (is_int($args[0])) {
         $depth = $args[0];
         unset($args[0]);
       }
-      if ($depth) error_log(debug_calling_function($depth));
+      if ($depth) error_log(debug_calling_function($depth)); //*/
+
+      if (is_int($args[0])) {
+        if ($depth=intval($args[0])) {
+          $args[0] = debug_calling_function($depth)
+        } else {
+          unset($args[0]);
+        }
+      } else {
+        error_log(debug_calling_function(1));
+      }
+
       foreach (func_get_args() as $message) {
         if (is_array($message) || is_object($message)) {
           error_log(print_r($message, true));
