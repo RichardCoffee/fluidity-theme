e4f931abda9d808671ef71773454d7e2b7d75f68
Laptop 3 <richard.coffee@gmail.com>
2016-08-09 18:23:14 -0400

    add sm css classes

--- single.php
+++ single.php
@@ -12,7 +12,7 @@ who_am_i(); ?>
 <div id="fluid-content" class="fluid-single <?php echo container_type('single'); ?>" <?php $micro->Blog(); ?>>
   <div class="row pad05perc">
     <div class="col-lg-12 col-md-12 col-sm-12 col-sx-12">
-      <div class="hidden-xs"><?php
+      <div class="hidden-sm hidden-xs"><?php
         sidebar_layout('single'); ?>
       </div>
       <div id="content" role="main" tabindex="-1"><?php
@@ -26,7 +26,7 @@ who_am_i(); ?>
           }
         } ?>
       </div><!-- #content -->
-      <div class="visible-xs"><?php
+      <div class="visible-sm visible-xs"><?php
         sidebar_layout('single'); ?>
       </div>
     </div><!-- col-*-12 -->
