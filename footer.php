da1b155605a2098c67713b2ca5bbc2aaf47b01dc
Laptop 3 <richard.coffee@gmail.com>
2016-08-30 04:18:25 -0400

    bugfix

--- footer.php
+++ footer.php
@@ -5,9 +5,9 @@
  *
  */
 
-global $micro; ?>
+ ?>
 
-<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php $micro->WPFooter(); ?>>
+<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
   <div class="row"><?php
     get_template_part('template-parts/footer',tcc_layout('footer'));
     wp_footer(); ?>
