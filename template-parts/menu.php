c16e243ffa70b32f66d0f8b48a64e23f8b16b907
Laptop 3 <richard.coffee@gmail.com>
2016-08-30 02:04:39 -0400

    move do_action

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -19,8 +19,8 @@ if (has_nav_menu('primary')) {
       <a class="navbar-brand " href="<?php echo esc_url( home_url() )?>"><?php bloginfo('name')?></a>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse"><?php /* Primary navigation */
-      wp_nav_menu( array('menu'=>'header','depth'=>2,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new wp_bootstrap_navwalker())); ?>
-    </div><?php
-    do_action('fluidity_menubar'); ?>
+      wp_nav_menu( array('menu'=>'header','depth'=>2,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new wp_bootstrap_navwalker()));
+      do_action('fluidity_menubar'); ?>
+    </div>
   </nav><?php
 }
