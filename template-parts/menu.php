12578604bd728d1f762bfb0674a3cc297e257e50
Laptop 3 <richard.coffee@gmail.com>
2016-11-04 15:55:16 -0400

    revert test

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -19,7 +19,7 @@ if (has_nav_menu('primary')) {
       <a class="navbar-brand " href="<?php echo esc_url( home_url() )?>"><?php bloginfo('name')?></a>
     </div>
     <div class="collapse navbar-collapse navbar-ex1-collapse"><?php /* Primary navigation */
-      wp_nav_menu( (object) array('menu'=>'header','depth'=>2,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new wp_bootstrap_navwalker()));
+      wp_nav_menu( array('menu'=>'header','depth'=>2,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new wp_bootstrap_navwalker()));
       do_action('fluidity_menubar'); ?>
     </div>
   </nav><?php
