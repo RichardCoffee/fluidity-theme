f7653ca757f37aad9b2246a92b9e810f677c034e
Laptop 3 <richard.coffee@gmail.com>
2016-11-30 02:35:04 -0500

    tracking

--- includes/in-the-loop.php
+++ includes/in-the-loop.php
@@ -69,6 +69,9 @@ if (!function_exists('fluid_post_date')) {
     $date   = get_the_date();
     $layout = tcc_settings('postdate');
 log_entry("Layout:  $layout");
+log_entry(0,"Modified Date:  ".get_the_modified_date('U'));
+log_entry(0,"Modified Calc:  ".(get_the_modified_date('U')-(60*60*24)));
+log_entry(0,"Post Date:      ".get_the_date('U'));
     if (($layout==='modified') && ((get_the_modified_date('U')-(60*60*24))>get_the_date('U'))) {
       $string = esc_html_x('Last modified on %1$s by %2$s','formatted date string, user name','tcc-fluid');
       $date   = get_the_modified_date();
