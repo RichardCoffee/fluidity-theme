9d8eef99cdc034c223f5d26f6cdd3972ce082f05
Laptop 3 <richard.coffee@gmail.com>
2016-12-06 23:43:16 -0500

    make color static

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -5,20 +5,18 @@
  *
  */
 
-if (has_nav_menu('primary')) {
-  $micro = microdata();
+$menu = 'primary';
+if (has_nav_menu($menu)) {
   $color = tcc_color_scheme(); ?>
-  <nav class="navbar navbar-<?php echo $color; ?>" <?php $micro->SiteNavigationElement(); ?> role="navigation">
+  <nav class="navbar navbar-<?php echo $color; ?>" <?php microdata()->SiteNavigationElement(); ?> role="navigation">
     <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-<?php echo $menu; ?>-collapse">
         <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
+        <i class="fa fa-bars"> </i>
       </button>
       <a class="navbar-brand " href="<?php echo esc_url( home_url() )?>"><?php bloginfo('name')?></a>
     </div>
-    <div class="collapse navbar-collapse navbar-ex1-collapse"><?php /* Primary navigation */
+    <div class="collapse navbar-collapse navbar-primary-collapse"><?php /* Primary navigation */
       wp_nav_menu( array('menu'=>'header','depth'=>2,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new wp_bootstrap_navwalker()));
       do_action('fluidity_menubar'); ?>
     </div>
