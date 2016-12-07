a47227301a8c3c89d1012d350987d841319f02b9
Laptop 3 <richard.coffee@gmail.com>
2016-12-07 01:29:11 -0500

    move action calls

--- functions.php
+++ functions.php
@@ -133,23 +133,35 @@ if (!function_exists('fluidity_show_query')) {
 /** Temporary Construction functions **/
 
 if (!is_child_theme()) {
-  function show_construction_title() { /*
+
+  add_action('tcc_header_top_menubar', 'fluidity_top_menubar');
+  add_action('tcc_top_right_menubar',  'fluidity_header_bar_login');
+  add_action('tcc_top_left_menubar',   'fluidity_social_icons');
+  add_action('tcc_header_body_content','fluidity_header_body');
+#  add_action('tcc_header_menubar',     'fluidity_main_menubar');
+  add_action('fluidity_menubar',       'fluidity_menubar_print_button');
+
+  if (get_theme_mod('header_logo') || tcc_design('logo')) {
+    add_action('tcc_top_left_menubar','fluidity_header_logo');
+  }
+  add_action('tcc_top_right_menubar','fluidity_main_menubar');
+
+ /*
+  function show_construction_title() {
     $site  = "<a href='".home_url()."' title='Fluidity'>This site</a>";
     $title = __('The Creative Collective');
     $refer = "<a href='http://the-creative-collective.com' target='TCC' title='$title'>$title</a>"; ?>
     <h1 class='text-center'><?php
       #echo "$site currently under construction by $refer";
       echo $title; ?>
-    </h1><?php //*/
-    remove_all_actions('tcc_header_menubar');
-    get_template_part('template-parts/menu');
-  }
+    </h1><?php
+  } //*
+
   #if (function_exists('jetpack_the_site_logo') || get_theme_mod('header_logo') || tcc_design('logo')) {
   if (get_theme_mod('header_logo') || tcc_design('logo')) {
     add_action('tcc_left_header_body','fluidity_header_logo');
-    add_action('tcc_right_header_body','show_construction_title');
+    add_action('tcc_right_header_body','fluidity_main_menubar');
   } else {
-    add_action('tcc_main_header_body','show_construction_title');
-  }
-  add_action('tcc_top_left_menubar','fluidity_social_icons');
+    add_action('tcc_main_header_body','fluidity_main_menubar');
+  } //*/
 }
