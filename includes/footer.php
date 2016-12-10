fb4504ce9134410ff8fe05bbcf8149c2e69787e8
Laptop 3 <richard.coffee@gmail.com>
2016-12-10 03:36:43 -0500

    add tcc-icon css class

--- includes/footer.php
+++ includes/footer.php
@@ -47,7 +47,7 @@ if (!function_exists('tcc_copyright')) {
 
 		<span class="pull-left">
 			<a href="http://the-creative-collective.com" target="the_creative_collective">
-				<img alt="TCC" src="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+				<img alt="TCC" src="<?php echo get_template_directory_uri(); ?>/favicon.ico" class="tcc-icon">
 			</a>
 		</span>
 
