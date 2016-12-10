d72f822192d66dba9ba73baf4441ac03770be15f
Laptop 3 <richard.coffee@gmail.com>
2016-12-10 01:28:38 -0500

    bugfix

--- includes/loader.php
+++ includes/loader.php
@@ -2,7 +2,6 @@
 
 function fluidity_class_loader( $class ) {
 	if (substr($class,0,4)==='TCC_') {
-		$dir  = dirname( TCCREP_PLUGIN_FILE );
 		$load = str_replace( '_', '/', substr( $class, (strpos($class,'_')+1) ) );
 		$file = FLUIDITY_HOME."classes/{$load}.php";
 		if ( is_readable( $file ) ) {
