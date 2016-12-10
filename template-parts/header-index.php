6049e9d6550bac32e8d3688b391980fbbcb2b981
Laptop 3 <richard.coffee@gmail.com>
2016-12-09 23:32:44 -0500

    change col

--- template-parts/header-index.php
+++ template-parts/header-index.php
@@ -8,10 +8,10 @@
 add_action('tcc_header_body_content','rtc_header_content');
 
 function rtc_header_content() { ?>
-	<div class="col-lg-4 col-md-3 col-sm-12 hidden-xs">
+	<div class="col-lg-3 col-md-2 col-sm-12 hidden-xs">
 		<?php fluidity_header_logo(); ?>
 	</div>
-	<div class="col-lg-8 col-md-9 col-sm-12 col-xs-12">
+	<div class="col-lg-9 col-md-10 col-sm-12 col-xs-12">
 		<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 			<div class="pull-right">
 				<?php fluidity_header_bar_login(true); ?>
