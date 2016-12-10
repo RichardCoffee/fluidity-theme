5db2c6e60b7c801a688ed2fc3a0558a2a2751b11
Laptop 3 <richard.coffee@gmail.com>
2016-12-09 23:47:15 -0500

    testing menu change

--- includes/hdr-tcc.php
+++ includes/hdr-tcc.php
@@ -5,14 +5,28 @@
  *
  */
 
-add_action('tcc_header_body_content', 'collective_top_menubar');
-add_action('tcc_top_right_menubar',  'collective_bar_login');
-add_action('tcc_top_right_menubar',  'fluidity_navbar_menu');
-add_action('tcc_top_left_menubar',   'fluidity_social_icons');
-if (get_theme_mod('header_logo') || tcc_design('logo')) {
-  add_action('tcc_top_left_menubar', 'collective_logo_limiter'); }
-add_action('tcc_top_left_menubar',   'fluidity_menubar_print_button');
+#add_action('tcc_header_body_content', 'collective_top_menubar');
+#add_action('tcc_top_right_menubar',  'collective_bar_login');
+#add_action('tcc_top_right_menubar',  'fluidity_navbar_menu');
+#add_action('tcc_top_left_menubar',   'fluidity_social_icons');
+#if (get_theme_mod('header_logo') || tcc_design('logo')) {
+#  add_action('tcc_top_left_menubar', 'collective_logo_limiter'); }
+#add_action('tcc_top_left_menubar',   'fluidity_menubar_print_button');
 
+add_action('tcc_header_menubar', 'collective_header');
+function collective_header() { ?>
+	<div class="pull-right">
+		<?php fluidity_menubar_print_button(); ?>
+	</div>
+	<div class="pull-right">
+		<?php fluidity_social_icons(); ?>
+	</div>
+	<div class="pull-right">
+		<?php fluidity_header_bar_login(true); ?>
+	</div>
+	<?php get_template_part('template-parts/menu');
+}
+/*
 function collective_top_menubar() { ?>
   <div class="row color-<?php echo $color; ?>">
     <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><?php
@@ -42,5 +56,5 @@ function tccollective_logo_limiter() {
     margin-right: 10%;
     margin-left: 10%;
   }\n";
-}
+} //*/
 #add_action('fluid_custom_css','tccollective_logo_limiter');
