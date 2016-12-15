607414ef98af2a66063ed0de641c6508f0e0a689
Laptop 3 <richard.coffee@gmail.com>
2016-12-15 13:13:38 -0500

    tracking

--- includes/loader.php
+++ includes/loader.php
@@ -3,7 +3,9 @@
 function fluidity_class_loader( $class ) {
 	if (substr($class,0,4)==='TCC_') {
 		$load = str_replace( '_', '/', substr( $class, (strpos($class,'_')+1) ) );
+log_entry("file: $load");
 		$file = FLUIDITY_HOME."classes/{$load}.php";
+log_entry(0,"path: $file");
 		if ( is_readable( $file ) ) {
 			include $file;
 		}
