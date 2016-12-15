95963721f1627660b6002fe8304ea68d732f089f
Laptop 3 <richard.coffee@gmail.com>
2016-12-15 10:25:07 -0500

    testing

--- index.php
+++ index.php
@@ -4,6 +4,32 @@
  *
  */
 
+
+if (!function_exists('log_entry')) {
+  function log_entry() {
+    if (WP_DEBUG) {
+      $args  = func_get_args();
+      $depth = 1;
+      if (is_int($args[0])) {
+        $depth = $args[0];
+        unset($args[0]);
+      }
+      if ($depth) error_log(debug_calling_function($depth));
+      foreach ($args as $message) {
+        if (is_array($message) || is_object($message)) {
+          error_log(print_r($message, true));
+        } else if ($message==='dump') {
+          error_log(print_r(debug_backtrace(),true));
+        } else {
+          error_log($message);
+        }
+      }
+    }
+  }
+} else { log_entry('log_entry already defined'); }
+  log_entry('log_entry now defined');
+
+
 log_entry('in index.php');
 
 get_header();
