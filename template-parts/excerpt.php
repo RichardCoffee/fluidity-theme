ee3ce186ab028e47cf3e4a7d19ac9a876e90e08a
Laptop 3 <richard.coffee@gmail.com>
2016-12-16 00:34:07 -0500

    show edit link on excerpts

--- template-parts/excerpt.php
+++ template-parts/excerpt.php
@@ -9,17 +9,17 @@ who_am_i(); ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php microdata()->BlogPosting(); ?>><?php
 
-  $format  = esc_html__('Permanent Link to %s','tcc-fluid');
-  $tooltip = sprintf($format,get_the_title()); ?>
+	$format  = esc_html__('Permanent Link to %s','tcc-fluid');
+	$tooltip = sprintf($format,get_the_title()); ?>
+	<h1 class="text-center" itemprop="headline">
+		<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php esc_attr_e($tooltip); ?>"><?php fluid_title(40); ?></a>
+		<?php fluid_edit_post_link(); ?>
+	</h1><?php
 
-  <h1 class="text-center" itemprop="headline">
-    <a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php esc_attr_e($tooltip); ?>"><?php fluid_title(40); ?></a>
-  </h1><?php
+	if (tcc_layout('exdate')==='show') { fluid_post_date(); } ?>
 
-  if (tcc_layout('exdate')==='show') { fluid_post_date(); } ?>
-
-  <div class="article" itemprop="description"><?php
-    the_excerpt(); ?>
-  </div>
+	<div class="article" itemprop="description"><?php
+		the_excerpt(); ?>
+	</div>
 
 </article>
