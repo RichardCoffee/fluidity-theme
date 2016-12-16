11ca2417433d202b06a369239ca2de5ad72dace4
Laptop 3 <richard.coffee@gmail.com>
2016-12-16 17:29:07 -0500

    add css class

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -49,7 +49,7 @@ if (!function_exists('fluidity_register_sidebars')) {
 	 #  Footer sidebar
     $sidebars[] = array('name'          => esc_html__('Front page Footer (2 col)','tcc-fluid'),
                         'id'            => 'footer2',
-                        'before_widget' => "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'><div class='panel panel-fluidity'><div class='panel-body'>",
+                        'before_widget' => "<div class='col-lg-6 col-md-6 col-sm-6 col-xs-12'><div class='panel panel-fluidity'><div class='panel-body back-fluidity'>",
                         'before_title'  => '',
                         'after_title'   => '',
                         'after_widget'  => "</div></div></div>");
