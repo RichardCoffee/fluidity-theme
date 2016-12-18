a69bfd628b7c6c1df9371694557823cb5274a62f
Laptop 3 <richard.coffee@gmail.com>
2016-12-18 17:19:09 -0500

    tracking

--- classes/Widgets/Basic.php
+++ classes/Widgets/Basic.php
@@ -10,7 +10,7 @@ class TCC_Widgets_Basic extends WP_Widget {
   function __construct($slug='',$title='',$desc=array()) {
     parent::__construct($this->slug,$this->title,array('description'=>$this->desc));
     if (!self::$micro && class_exists('TCC_Microdata')) self::$micro = TCC_Microdata::get_instance();
-log_entry('initilized: '.get_class());
+log_entry('initilized: '.get_class().' '.__CLASS__);
   }
 
   public function widget($args,$instance) {
