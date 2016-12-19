ab351e97a35769bb9cdeda032ee5458d652156b6
Laptop 3 <richard.coffee@gmail.com>
2016-12-18 22:49:52 -0500

    add footer tag

--- footer.php
+++ footer.php
@@ -7,13 +7,15 @@
 
 require_once('includes/footer.php'); ?>
 
-<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
-  <div class="row"><?php
-    $slug = fluidity_page_slug();
-    get_template_part('template-parts/footer',$slug);
-    wp_footer(); ?>
-  </div>
-</div>
+<footer>
+	<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
+		<div class="row"><?php
+			$slug = fluidity_page_slug();
+			get_template_part('template-parts/footer',$slug);
+			wp_footer(); ?>
+		</div>
+	</div>
+</footer>
 
 </body>
 
