1a2f2e47933db59c54e43a055c9713aa17ad0abc
James G <james.gaither@the-creative-collective.com>
2016-12-22 17:20:17 -0500

    changes to files

--- design/js/scrolleffects.js
+++ design/js/scrolleffects.js
@@ -47,7 +47,7 @@ function update(){
     $('.w-scroll').each(function() { 
         var $element = $(this);
         // subtract some from the height b/c of the padding
-      
+        var height = $element.height()-18;
         $(this).css('backgroundPosition', '50% ' + Math.round((height - pos) * velocity) + 'px'); 
     }); 
 
