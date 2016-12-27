40a7a914cd61aa03e39a8b320a141966b507d02b
Laptop 3 <richard.coffee@gmail.com>
2016-12-27 18:49:38 -0500

    add check for fixed header

--- js/header-fixed.js
+++ js/header-fixed.js
@@ -2,4 +2,5 @@
 jQuery(document).ready(function (){
 	var divHeight = jQuery('.header-fixed').height();
 	jQuery('main').css('margin-top', divHeight+'px');
+	if (typeof collapse != 'undefined') { collapse.fixed = true; } // collapse declared in js/collapse.js
 });
