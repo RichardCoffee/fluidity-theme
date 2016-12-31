a0917b12fe007e7f22c379e9fd8ef9cd4d84a174
Laptop 3 <richard.coffee@gmail.com>
2016-12-31 15:21:28 -0500

    testing dual navigation

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,25 +11,28 @@ who_am_i(); ?>
 
 <article id="post-<?php the_ID(); ?> " <?php post_class(); ?> <?php microdata()->BlogPosting(); ?>>
 
-  <?php fluid_thumbnail();
+	<?php fluid_thumbnail();
 
-  if (!is_page()) { ?>
+	if (!is_page()) { ?>
 
-    <h1 class="text-center">
-		<?php tcc_post_title(); ?>
-      <?php fluid_edit_post_link(); ?>
-    </h1><?php
+		<h1 class="text-center">
+			<?php tcc_post_title(); ?>
+			<?php fluid_edit_post_link(); ?>
+		</h1><?php
 
-    fluid_post_date(true);
-  } ?>
+		fluid_post_date(true);
+	} ?>
 
-  <div class="article" itemprop="articleBody">
-    <?php the_content(); ?>
-  </div><?php
+	<div class="article" itemprop="articleBody">
+		<?php the_content(); ?>
+	</div><?php
 
-  if (is_single()) {
-    fluid_navigation();
-    fluid_postmetadata();
-  } ?>
+	if (is_single()) {
+		$mycat = '';
+		$cats  = get_the_category();
+		if ($cats) { $mycat = $cats[0]->slug; }
+		fluid_navigation($mycat,true);
+		fluid_postmetadata();
+	} ?>
 
 </article>
