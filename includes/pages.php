20f829f4d941db57ce346916d23ff3d7b96ed021
Laptop 3 <richard.coffee@gmail.com>
2017-01-08 01:51:13 -0500

    bugfix

--- includes/pages.php
+++ includes/pages.php
@@ -59,6 +59,7 @@ if (!function_exists('get_page_slug')) {
          global $fluidity_theme_template; // FIXME: this is not a reliable source
          $page = get_queried_object();
 //if (!is_object($page)) { log_entry('bad page value',$page,$fluidity_theme_template,$_SERVER); }
+// FIXME: test for post_type property?
          if (is_object($page) && ($page->post_type==='page')) {
             $slug = $page->post_name;
          } else {
