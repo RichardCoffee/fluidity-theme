841b4c8e77b1e6362ac112e07e68f86b89de1736
James G <james.gaither@the-creative-collective.com>
2017-01-10 04:09:22 -0500

    favicon

--- footer.php
+++ footer.php
@@ -11,9 +11,9 @@
 	<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
 		<?php get_template_part('template-parts/footer',get_page_slug()); ?>
 	</div>
-	<?php wp_footer(); ?>
+	
 </footer>
-
+<?php wp_footer(); ?>
 </body>
 
 </html>
