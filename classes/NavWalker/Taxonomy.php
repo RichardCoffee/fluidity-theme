b58d124a39841d3454ba5ee0d2757502ae43ad4f
Laptop 3 <richard.coffee@gmail.com>
2017-01-11 01:22:26 -0500

    add NavWalker classes, tracking

---
+++ classes/NavWalker/Taxonomy.php
@@ -0,0 +1,11 @@
+<?php
+
+class TCC_NavWalker_Taxonomy extends Walker_Nav_Menu {
+
+	public function display_element( $element, &$children_elements, $max_depth, $depth, $args, &$output ) {
+log_entry(func_get_args());
+		if (!$element) return;
+		parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
+	}
+
+}
