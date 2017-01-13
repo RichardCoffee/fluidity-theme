e5e51425ebecbf47723357e12e4bc4ad758d9f8e
Laptop 3 <richard.coffee@gmail.com>
2017-01-13 00:15:22 -0500

    add css/columns.css

--- functions.php
+++ functions.php
@@ -57,6 +57,7 @@ if (!function_exists('tcc_enqueue')) {
     wp_register_style('fa-social',      get_theme_file_uri("css/fa-social-hover.css"), array('tcc-fawe'), FLUIDITY_VERSION);
     wp_register_style('fluidity',       get_theme_file_uri("style.css"),               null,              FLUIDITY_VERSION);
     wp_register_style('tcc-reduce-css', get_theme_file_uri("css/header-reduce.css"),   null,              FLUIDITY_VERSION);
+    wp_register_style('tcc-columns',    get_theme_file_uri("css/columns.css",          null,              FLUIDITY_VERSION);
     wp_enqueue_style('tcc-fawe');	#  font-awesome needs to be loaded before bootstrap, due to css conflict (sr-only)
     if (tcc_option('active','social')==='yes') {
 		 wp_enqueue_style('fa-social'); }
