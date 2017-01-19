9c0e6c081cf0881f9009588634e9323c2c2c7b35
Laptop 3 <richard.coffee@gmail.com>
2017-01-19 07:02:24 -0500

    testing

--- includes/pages.php
+++ includes/pages.php
@@ -57,16 +57,18 @@ if (!function_exists('get_page_slug')) {
 	#	http://www.wpaustralia.org/wordpress-forums/topic/pre_get_posts-and-is_front_page/
 	function get_page_slug() {
 		global $wp_query;
-		static $slug = null;
-		if (!$slug) {
-			if ( !is_admin() && $wp_query->is_main_query() ) {
 				global $fluidity_theme_template; // FIXME: this is not a reliable source
+$slug = $fluidity_theme_template;
+#		static $slug = null;
+#		if (!$slug) {
+			if ( !is_admin() && $wp_query->is_main_query() ) {
 				if ( is_home() && empty( $wp_query->query_string ) ) {
 					$slug = 'blog';
 				} else if ( ( $wp_query->get( 'page_id' ) == get_option( 'page_on_front' ) && get_option( 'page_on_front' ) ) || empty( $wp_query->query_string ) ) {
 					$slug = 'front';
 				} else {
 					$page = get_queried_object(); // wtf?
+#log_entry($page);
 					if (is_object($page) && !isset($page->post_type)) { log_entry('bad post type value',$fluidity_theme_template,'class: '.get_class($page),$page); }
 					if (is_object($page) && isset($page->post_type) && ($page->post_type==='page')) {
 						$slug = $page->post_name;
@@ -76,8 +78,8 @@ if (!function_exists('get_page_slug')) {
 					}
 				}
 			}
-		}
 log_entry($slug.':  '.debug_calling_function());
+#		}
 		return $slug;
 	}
 }
