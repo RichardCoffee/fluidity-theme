5674a9091a520278bbb30ce3530052afe8cc8d34
Laptop 3 <richard.coffee@gmail.com>
2017-01-19 06:53:07 -0500

    fix type hint

--- footer.php
+++ footer.php
@@ -11,8 +11,8 @@
 	<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
 		<?php get_template_part('template-parts/footer',get_page_slug()); ?>
 	</div>
-	
 </footer>
+
 <?php wp_footer(); ?>
 </body>
 
