4182427cfae0a817f9f3b701b5d5ba75607b4fe2
Laptop 3 <richard.coffee@gmail.com>
2017-01-20 06:21:05 -0500

    remove tracking code

--- footer.php
+++ footer.php
@@ -9,7 +9,6 @@
 
 <footer>
 	<div id="fluid-footer" class="<? echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
-<?php log_entry(get_page_slug()); ?>
 		<?php get_template_part('template-parts/footer',get_page_slug()); ?>
 	</div>
 </footer>
