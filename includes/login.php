432c9ca288c49feb18b1a6d2cedb027dde430730
Laptop 3 <richard.coffee@gmail.com>
2017-01-21 18:57:24 -0500

    tracking

--- includes/login.php
+++ includes/login.php
@@ -25,6 +25,7 @@ if (!function_exists('tcc_login_redirect')) {
 }
 
 if (!function_exists('tcc_admin_login_redirect')) {
+log_entry('tcc_admin_login_redirect');
 	function tcc_admin_login_redirect($redirect_to,$request,$user) {
 log_entry($redirect_to,$request,$user,wp_get_referer());
 #		if (!$user)                       { return home_url(); }
