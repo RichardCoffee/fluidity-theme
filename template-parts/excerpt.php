23b5178c55089d7ddb0c61b78a6a78c4d3daf5c1
Laptop 3 <richard.coffee@gmail.com>
2017-01-21 21:17:51 -0500

    add h2 tag

--- template-parts/excerpt.php
+++ template-parts/excerpt.php
@@ -15,7 +15,11 @@ who_am_i(); ?>
 		<?php tcc_post_title(40); ?>
 	</h1>
 
-	<?php if (tcc_layout('exdate')==='show') { fluid_post_date(); } ?>
+	<?php if (tcc_layout('exdate')==='show') { ?>
+		<h2 class="text-center">
+			<?php fluid_post_date(); ?>
+		</h2><?php
+	} ?>
 
 	<div class="article" itemprop="description"><?php
 		the_excerpt(); ?>
