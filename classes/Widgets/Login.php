117199224b623a2bb038cfc0efd383ac3b21153f
Laptop 3 <richard.coffee@gmail.com>
2017-01-21 21:05:58 -0500

    revert widget

--- classes/Widgets/Login.php
+++ classes/Widgets/Login.php
@@ -2,25 +2,15 @@
 
 class TCC_Widgets_Login extends TCC_Widgets_Basic {
 
-	private $wp_login_defaults;
-
 	function __construct() {
 		$this->title = esc_html__('Login','tcc-fluid');
 		$this->desc  = esc_html__('Fluidity Login form','');
 		$this->slug  = 'tcc_login';
 		parent::__construct();
-		add_filter('login_form_defaults',array($this,'login_form_defaults'),1000);
-		$temp = wp_login_form(array('echo'=>false));
-log_entry($temp);
 	}
 
 	public function inner_widget($args,$instance) {
-		tcc_login_form($this->wp_login_defaults);
-	}
-
-	public function login_form_defaults($args) {
-		$this->wp_login_defaults = $args;
-		return $args;
+		tcc_login_form();
 	}
 
 }
