56a39b9760181182d92a0b479df574b0cf6d3df6
Laptop 3 <richard.coffee@gmail.com>
2017-01-24 06:06:09 -0500

    bugfix

--- includes/widgets.php
+++ includes/widgets.php
@@ -2,7 +2,7 @@
 
 if (!function_exists('tcc_register_widgets')) {
 	function tcc_register_widgets() {
-		$widgets = aaply_filters( 'tcc_register_widgets_list', array('Address','Login','Logo','Search') );
+		$widgets = apply_filters( 'tcc_register_widgets_list', array('Address','Login','Logo','Search') );
 		foreach($widgets as $widget) {
 			register_widget("TCC_Widgets_$widget");
 		}
