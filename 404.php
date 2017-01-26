4a1b8e881bf5f662c2dd711f0766ada9f0c678bb
Laptop 3 <richard.coffee@gmail.com>
2017-01-26 08:19:04 -0500

    reformat 404 page

--- 404.php
+++ 404.php
@@ -6,44 +6,44 @@ defined( 'ABSPATH' ) || exit;
 
 get_header();
 
-$micro = microdata();
+$mypage = '404';
 
 if (WP_DEBUG && is_user_logged_in() && current_user_can('update_core')) { ?>
- <div class="panel panel-fluidity collapse-auto">
-   <div class="panel-heading">
-      <h3 class="panel-title">Word Press Information</h3>
-    </div>
-    <div class="panel-body">
-      <pre><?php
-        print_r($_GET);
-        print_r($_POST);
-        print_r($wp_query); ?>
-      </pre><?php
-      debug_rewrite_rules(); ?>
-    </div>
-  </div><?php
+	<div class="panel panel-fluidity collapse-auto">
+		<div class="panel-heading">
+			<h3 class="panel-title">Word Press Information</h3>
+		</div>
+		<div class="panel-body">
+			<pre><?php
+				print_r($_GET);
+				print_r($_POST);
+				print_r($wp_query); ?>
+			</pre><?php
+			debug_rewrite_rules(); ?>
+		</div>
+	</div><?php
 } //*/ ?>
 
-<div class="row text-center">
-  <div class="col-md-12">
-    <article>
-      <h1  class="page-title"><?php esc_html_e(__("Ooops.....Well this is somewhat embarrassing, isn't it?",'tcc-fluid')); ?></h1>
-      <div class="page-content">
-        <h2><?php esc_html_e(__('It seems as if the page you are looking for is not here','tcc-fluid')); ?></h2>
-        <h3><?php esc_html_e(__('It looks like nothing was found at this location','tcc-fluid')); ?></h3>
-        <div class="row">
-          <div class="col-lg-4 col-md-3 col-sm-2 hidden-xs"></div>
-          <div class="col-lg-4 col-md-6 col-sm-8 col-xs-12">
-            <?php get_search_form(); ?>
-          </div>
-          <div class="col-lg-4 col-md-3 col-sm-2 hidden-xs"></div>
-        </div>
-        <h1>
-          <a href="<?php echo home_url() ; ?>"><?php esc_html_e(__('Home Page','tcc-fluid')); ?></a>
-        </h1>
-      </div><!-- .page-content -->
-    </article>
-  </div><!-- .col-md-12 -->
-</div><!-- .row --><?php
+<div class="<?php container_type($mypage); ?>">
+	<div class="row text-center">
+		<div class="col-md-12">
+			<article>
+				<h1  class="page-title"><?php esc_html_e(__("Ooops.....Well this is somewhat embarrassing, isn't it?",'tcc-fluid')); ?></h1>
+				<div class="page-content">
+					<h2><?php esc_html_e(__('It seems as if the page you are looking for is not here','tcc-fluid')); ?></h2>
+					<h3><?php esc_html_e(__('It looks like nothing was found at this location','tcc-fluid')); ?></h3>
+					<div class="row">
+						<div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
+							<?php get_search_form(); ?>
+						</div>
+					</div>
+					<h1>
+						<a href="<?php echo home_url() ; ?>"><?php esc_html_e(__('Home Page','tcc-fluid')); ?></a>
+					</h1>
+				</div><!-- .page-content -->
+			</article>
+		</div><!-- .col-md-12 -->
+	</div><!-- .row -->
+</div><!-- .<?php container_type($mypage); ?> --><?php
 
 get_footer(); ?>
