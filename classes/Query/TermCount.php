d280937c14213df04cf50fc1292514f37e55f4f7
Laptop 3 <richard.coffee@gmail.com>
2017-01-29 00:36:30 -0500

    tweaking

--- classes/Query/TermCount.php
+++ classes/Query/TermCount.php
@@ -18,19 +18,16 @@ class TCC_Query_TermCount {
 		$this->parse_args( $args );
 		$this->get_taxonomy_count();
 		if ( ! empty( $this->counts ) ) {
-			add_filter( 'wp_nav_menu_objects', array( $this, 'tcc_navmenu_catagory_count' ) ); }
+			add_filter( 'wp_nav_menu_objects', array( $this, 'add_navmenu_catagory_count' ) ); }
 	}
 
-	public function tcc_navmenu_catagory_count( $items ) {
+	public function add_navmenu_catagory_count( $items ) {
 		foreach( $items as $item ) {
 			#      Operate on children only                       Check to see if the menu item is in our taxonomy
 			if ( ( intval( $item->menu_item_parent, 10 ) > 0 ) && array_key_exists( $item->object_id, $this->counts ) ) {
 				$css = ( $this->counts[ $item->object_id ] === 0 ) ? 'no-count' : 'count';
-				$css.= " term-count term-count-{$this->taxonomy}";
-				$item->title = sprintf( '%s <span class="%s">%s</span>',
-				                        $item->title,
-				                        esc_attr($css),
-				                        number_format_i18n( $this->counts[ $item->object_id ] ) );
+				$css.= ' term-count term-count-' . $this->taxonomy;
+				$item->title = sprintf( '%s <span class="%s">%s</span>', $item->title, esc_attr($css), number_format_i18n( $this->counts[ $item->object_id ] ) );
 			}
 		}
 		return $items;
