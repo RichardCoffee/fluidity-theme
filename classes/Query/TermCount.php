dc8df45c6cef14e4119f97ce2210af41092d864a
Laptop 3 <richard.coffee@gmail.com>
2017-01-28 23:38:06 -0500

    parse term array

--- classes/Query/TermCount.php
+++ classes/Query/TermCount.php
@@ -11,24 +11,35 @@ class TCC_Query_TermCount {
 
 	protected $taxonomy = 'category';
 
-	protected static $terms;
+	protected static $terms = array();
 
 	use TCC_Trait_ParseArgs;
 
 	public function __construct( $args = array() ) {
 		$this->parse_args( $args );
-		self::$terms = get_terms( array( 'taxonomy' => $this->taxonomy, 'hide_empty' => false, ) );
+		$this->get_taxonomy_count();
 log_entry(self::$terms);
 		add_filter( 'wp_nav_menu_objects', array( $this, 'tcc_navmenu_catagory_count' ) );
 	}
 
 	public function tcc_navmenu_catagory_count( $items ) {
-		foreach( $items as $item ) {
-			if ( $item->object === $this->taxonomy ) {
-				log_entry($item);
+		if (self::$terms) {
+			foreach( $items as $item ) {
+				if ( $item->object === $this->taxonomy ) {
+					log_entry($item);
+				}
 			}
 		}
 		return $items;
+	}
+
+	private function get_taxonomy_count() {
+		$terms = get_terms( array( 'taxonomy' => $this->taxonomy, 'hide_empty' => false, ) );
+		if ( $terms ) {
+			foreach( $terms as $term ) {
+				self::$terms[ $term->slug ] = $term->count;
+			}
+		}
 	}
 
 
