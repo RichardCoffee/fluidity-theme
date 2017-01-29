60a0b47309b24729528eed483b35f104ff299a58
Laptop 3 <richard.coffee@gmail.com>
2017-01-29 00:13:51 -0500

    add count span

--- classes/Query/TermCount.php
+++ classes/Query/TermCount.php
@@ -25,8 +25,14 @@ class TCC_Query_TermCount {
 	public function tcc_navmenu_catagory_count( $items ) {
 		if (self::$terms) {
 			foreach( $items as $item ) {
-#				#      Operate on children only                       Check to see if the menu item is in our taxonomy
+				#      Operate on children only                       Check to see if the menu item is in our taxonomy
 				if ( ( intval( $item->menu_item_parent, 10 ) > 0 ) && array_key_exists( $item->object_id, self::$terms ) ) {
+					$css = "term-count term-count-{$this->type}";
+					$css.= ( self::$terms[ $item->object_id ] === 0 ) ? ' no-count' : ' count';
+					$item->title = sprintf( '%s <span class="%s">%s</span>',
+					                        $item->title,
+					                        esc_attr($css),
+					                        number_format_i18n( self::$terms[ $item->object_id ] ) );
 					log_entry($item);
 				}
 			}
