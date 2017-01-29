b5505453db7c4099ee163221c622af8713697388
Laptop 3 <richard.coffee@gmail.com>
2017-01-29 02:46:02 -0500

    show sidebar slug

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -91,6 +91,7 @@ if (!function_exists('fluidity_load_sidebar')) {
     foreach($sidebars as $sidebar) {
       if (is_active_sidebar($sidebar)) {
         if (dynamic_sidebar($sidebar)) {
+          if (tcc_settings('where')==='on') { echo "<p>$sidebar active</p>"; }
           return true;
         } #else { echo "<p>$sidebar non-dynamic</p>"; }
       } #else {   echo "<p>$sidebar not active</p>";  }
