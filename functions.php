333618a5c0cb1470e33a5517416c2f0f67bfc956
Laptop 3 <richard.coffee@gmail.com>
2017-01-29 02:09:03 -0500

    comment class

--- functions.php
+++ functions.php
@@ -42,7 +42,7 @@ if (is_admin()) {
   require_once('includes/pages.php');
   require_once('includes/third-party.php');
   require_once('classes/microdata.php');
-	new TCC_Query_TermCount();
+#	new TCC_Query_TermCount( array( 'taxonomy' => 'category' ) );
 }
 
 if (!function_exists('tcc_enqueue')) {
