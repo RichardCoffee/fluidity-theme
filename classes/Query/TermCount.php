c27a83ee1a61a2940c7d544f2749aa9b98fb6a6e
Laptop 3 <richard.coffee@gmail.com>
2017-01-28 23:57:14 -0500

    change term chaeck

--- classes/Query/TermCount.php
+++ classes/Query/TermCount.php
@@ -18,15 +18,15 @@ class TCC_Query_TermCount {
 	public function __construct( $args = array() ) {
 		$this->parse_args( $args );
 		$this->get_taxonomy_count();
-log_entry(self::$terms);
+#log_entry(self::$terms);
 		add_filter( 'wp_nav_menu_objects', array( $this, 'tcc_navmenu_catagory_count' ) );
 	}
 
 	public function tcc_navmenu_catagory_count( $items ) {
 		if (self::$terms) {
 			foreach( $items as $item ) {
-				#    Check for taxonomy                     Children only
-				if ( $item->object === $this->taxonomy && ( intval( $item->menu_item_parent, 10 ) > 0 ) ) {
+#				#    Check to see if the menu item is in our taxonomy
+				if ( array_key_exists( $item->object_id, self::$terms ) ) {
 					log_entry($item);
 				}
 			}
@@ -36,7 +36,7 @@ log_entry(self::$terms);
 
 	private function get_taxonomy_count() {
 		$terms = get_terms( array( 'taxonomy' => $this->taxonomy, 'hide_empty' => false, ) );
-log_entry($terms);
+#log_entry($terms);
 		if ( $terms ) {
 			foreach( $terms as $term ) {
 				self::$terms[ $term->term_id ] = $term->count;
