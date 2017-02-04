b134345260646f2e710b296408905ec3ba0618c0
Laptop 3 <richard.coffee@gmail.com>
2017-02-04 13:21:58 -0500

    comment code

---
+++ includes/cookies.php
@@ -0,0 +1,20 @@
+<?php
+
+if ( ! function_exists( 'list_cookies' ) ) {
+	#	https://artiss.blog/2012/05/wordpress-function-to-list-site-cookies/
+	function list_cookies( $paras = '', $content = '' ) {
+		$novalue   = ( strtolower( $paras[ 0 ] ) === 'novalue' ) ? true : false;
+		$separator = ( empty( $content ) ) ? ' : ' : $content;
+		$cookie    = $_COOKIE;
+		ksort( $cookie );
+		$content = '<ul class="cookie-list">';
+		foreach ( $cookie as $key => $val ) {
+			$content .= '<li class="cookie-list-item">' . $key;
+			$content .= ( ! $novalue ) ? $separator . $val : '';
+			$content .= "</li>";
+		}
+		$content .= "</ul>";
+		return do_shortcode( $content );
+	}
+	add_shortcode( 'cookies', 'list_cookies' );
+}
