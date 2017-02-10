4aef3638e1cbee0f0178308a2bf705c5e323f71e
Laptop 3 <richard.coffee@gmail.com>
2017-02-10 12:21:47 -0500

    fix footer

--- footer.php
+++ footer.php
@@ -8,16 +8,17 @@
  ?>
 
 		<footer>
+
 			<div id="fluid-footer" class="<?php echo container_type('footer'); ?>" <?php microdata()->WPFooter(); ?>>
-<p>before footer</p>
 				<?php get_template_part('template-parts/footer',get_page_slug()); ?>
-<p>after footer</p>
 			</div>
+
+			<div id="wp-footer">
+				<?php wp_footer(); ?>
+			</div>
+
 		</footer>
 
-		<div id="wp-footer">
-			<?php wp_footer(); ?>
-		</div>
-
 	</body>
+
 </html>
