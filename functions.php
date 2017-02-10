2c49ec852fb2f97d2f04eeb21d332726e2f39a2e
Laptop 3 <richard.coffee@gmail.com>
2017-02-09 23:02:41 -0500

    force update

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
  */
 
 //this is basically just a header body class that is required for some odd reason to make the theme complete in wordpress's theme authoring test thingy...jlg
-if (!isset($content_width)) $content_width = 1600;
+if ( ! isset( $content_width ) ) $content_width = 1600;
 
 define( 'FLUIDITY_HOME', trailingslashit( dirname( __FILE__ ) ) );  #  get current directory
 $data = get_file_data( FLUIDITY_HOME . 'style.css', array( 'ver' => 'Version' ) );
