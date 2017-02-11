0c0c5104177933e9fc10a67dccb315d46479edc8
Laptop 3 <richard.coffee@gmail.com>
2017-02-11 16:39:00 -0500

    bugfix

--- classes/Form/Field/Field.php
+++ classes/Form/Field/Field.php
@@ -27,7 +27,7 @@ abstract class TCC_Form_Field_Field {
 	public function __construct( $args ) {
 		$this->parse_args( $args );
 		if ( ( empty( $this->placeholder ) ) && ( ! empty( $this->label_text ) ) ) {
-			$this->placeholder = $this->field_text;
+			$this->placeholder = $this->label_text;
 		}
 		if ( empty( $this->field_id ) ) {
 			$this->field_id = $this->field_name;
