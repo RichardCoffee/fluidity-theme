1ff42289515036a97ad51b90f71549b385dbc527
Laptop 3 <richard.coffee@gmail.com>
2017-02-17 21:19:36 -0500

    tracking

--- includes/sidebars.php
+++ includes/sidebars.php
@@ -173,10 +173,10 @@ if ( ! function_exists( 'tcc_sidebar' ) ) {
 	function tcc_sidebar( $sidebar = 'standard', $extra_css = array() ) {
 		if ( defined( 'TCC_NO_SIDEBAR' ) ) { return; }  #  define in page template file
 		$side = tcc_layout( 'sidebar' );
-log_entry("side:  $side");
 		defined( 'TCC_LEFT_SIDEBAR' )  or ( $side = 'left' );
 		defined( 'TCC_RIGHT_SIDEBAR' ) or ( $side = 'right' );
 		if ( $side !== 'none' ) {
+log_entry("side:  $side");
 			$slug = get_page_slug();
 			$css  = array(
 				'widget-area',
