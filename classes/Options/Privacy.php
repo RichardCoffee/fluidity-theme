e8171f089d173985264fa8bebbd08a2b43ad6fa3
Laptop 3 <richard.coffee@gmail.com>
2017-02-21 13:32:52 -0500

    tracking

--- classes/Options/Privacy.php
+++ classes/Options/Privacy.php
@@ -207,6 +207,7 @@ class TCC_Options_Privacy {
 	private function get_theme_list() {
 		$theme_list = array();
 		foreach( $this->themes as $slug => $theme ) {
+log_entry($slug);
 			if ( strpos( 'twenty', $slug ) === 0 ) {
 				continue;  #  Do not filter wordpress themes
 			}
--- tests/struct.php
+++ tests/struct.php
@@ -2,8 +2,11 @@
 
 $test = test_one() || test_two();
 
-echo $test;
+$test2 = ( test_one() ) ? test_one() : test_two();
+
+echo "$test\n";
+echo "$test2\n";
 
 
-function test_one() { return 'item one'; }
+function test_one() { return ''; }
 function test_two() { return 'item two'; }
