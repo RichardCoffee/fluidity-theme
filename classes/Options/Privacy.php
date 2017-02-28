6e39bbfe526b2bee4043f4dc7cd49f3f632e0a5a
Laptop 3 <richard.coffee@gmail.com>
2017-02-27 19:52:16 -0500

    move text

--- classes/Options/Privacy.php
+++ classes/Options/Privacy.php
@@ -178,12 +178,12 @@ class TCC_Options_Privacy {
 log_entry($active_list, $this->plugins );
 		foreach ( $this->plugins as $path => $plugin ) {
 			$title = '<a href="' . esc_attr( $plugin['PluginURI'] ) . '" target="' . esc_attr( $path ) . '">';
-			$title.= esc_html( $plugin['Name'] ) . '</a> by ';
-			$title.= '<a href="' . esc_attr( $plugin['AuthorURI'] ) . '" target="' . sanitize_title( $plugin['Author'] ) . '">';
-			$title.= esc_html( $plugin['Author'] ) . '</a>';
+			$title.= esc_html( $plugin['Name'] ) . '</a>'
 			if ( in_array( $path, $active_list ) ) {
-				$title .= ' <span class="red">active</span>';
+				$title .= ' <span class="red">(active)</span>';
 			}
+			$title.= ' by <a href="' . esc_attr( $plugin['AuthorURI'] ) . '" target="' . sanitize_title( $plugin['Author'] ) . '">';
+			$title.= esc_html( $plugin['Author'] ) . '</a>';
 			$plugin_list[ $path ] = $title;
 		}
 		return $plugin_list;
