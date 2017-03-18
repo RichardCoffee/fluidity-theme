6a063152424445cb7a9f76cd0e5c61c68a474388
Laptop 3 <richard.coffee@gmail.com>
2017-03-17 22:21:42 -0400

    bugfix - fix property reference, change metabox priority

--- classes/MetaBox/PostDate.php
+++ classes/MetaBox/PostDate.php
@@ -2,11 +2,12 @@
 
 class TCC_MetaBox_PostDate extends TCC_MetaBox_MetaBox {
 
-	protected $context = 'side';
-	protected $field   = 'postdate_display';
-	protected $nonce   = 'postdate_meta_box';
-	protected $radio   = null;  #  object of type TCC_Form_Field_Radio
-	protected $slug    = 'postdate_meta_box';
+	protected $context  = 'side';
+	protected $field    = 'postdate_display';
+	protected $nonce    = 'postdate_meta_box';
+	protected $priority = 'default';
+	protected $radio    = null;  #  object of type TCC_Form_Field_Radio
+	protected $slug     = 'postdate_meta_box';
 
 	public function __construct( $args = array() ) {
 		$this->title = __( 'Displayed Post Date', 'tcc-fluid' );
