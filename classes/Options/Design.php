25915072cdee2fb98de223d8e85db95e9cea39f4
Laptop 3 <richard.coffee@gmail.com>
2017-04-25 20:12:17 -0400

    convert design options to use base options class

--- classes/Options/Design.php
+++ classes/Options/Design.php
@@ -1,94 +1,67 @@
 <?php
 
-class TCC_Options_Design {
+class TCC_Options_Design extends TCC_Options_Options {
 
-  private $base     = 'design';
-  private $priority = 34; # customizer priority
 
-  public function __construct() {
-    add_filter('fluidity_options_form_layout', array($this,'form_layout'), $this->priority );
-    add_action('fluid-customizer', array($this,'options_customize_register'),$this->priority,2);
-  }
+	protected $base     = 'design';
+	protected $priority = 34; # customizer priority
 
-  private function form_title() {
+
+  protected function form_title() {
     return __('Design','tcc-fluid');
   }
 
-  public function form_layout($form) {
-    $form[$this->base] = array('describe' => array($this,'describe_options'),
-                               'title'    => $this->form_title(),
-                               'option'   => 'tcc_options_'.$this->base,
-                               'layout'   => $this->options_layout());
-    return $form;
-  }
-
-  public function describe_options() { ?>
+	public function describe_options() { ?>
 		<span title="No they don't.  This doesn't work.  yet">
 			<?php esc_html_e('Design Options - these options also show up in the WordPress Customizer.','tcc-fluid'); ?>
 		</span><?php
-  }
+	}
 
-  protected function options_layout() {
-    $layout = array('default'=>true); /*
-    $layout['logo']   = array('default' => '',
-                              'label'   => __('Theme Logo','tcc-fluid'),
-                              'render'  => 'image',
-                              'divcss'  => 'upload-img',
-                              'media'   => array('title'  => __('Assign/Upload Theme Logo','tcc-fluid'),
-                                                 'button' => __('Assign Logo', 'tcc-fluid'),
-                                                 'delete' => __('Remove Logo', 'tcc-fluid'))); //*/
-    $layout['paral']  = array('default' => 'no',
-                              'label'   => __('Parallax','tcc-fluid'),
-                              'text'    => __("Do you want to use the parallax effect for a page's featured image?",'tcc-fluid'),
-                              'help'    => "No idea why this is called parallax, but it sounds very scientific.",
-                              'render'  => 'radio',
-                              'source'  => array('no'  => __('Show featured images normally - although currently this is not at all on pages','tcc-fluid'),
-                                                 'yes' => __('Use featured image as background','tcc-fluid')));
-    $layout['title']  = array('default' => 'no',
-                              'label'   => __( 'Page Title', 'tcc-fluid' ),
-                              'text'    => __( 'Do you want to show the page title before the content?', 'tcc-fluid' ),
-                              'render'  => 'radio',
-                              'source'  => array('no'  => __( 'Do not show the page title.', 'tcc-fluid' ),
-                                                 'yes' => __( 'Show the page title after the header and before the content.', 'tcc-fluid' ) ) );
-    $layout['type']   = array('label'   => __('Typography','tcc-fluid'),
-                              'text'    => __('Site typography options','tcc-fluid'),
-                              'render'  => 'title');
-    $layout['font']   = array('default' => 'Helvitica Neue',
-                              'label'   => __('Font Type','tcc-fluid'),
-                              'render'  => 'font',
-                              'source'  => TCC_Options_Typography::mixed_fonts());
-    $layout['size']   = array('default' => 18,
-                              'label'   => __('Font Size','tcc-fluid'),
-                              'stext'   => _x('px',"abbreviation for 'pixel'",'tcc-fluid'),
-                              'render'  => 'text',
-                              'divcss'  => 'tcc_text_3em');
-/*    $layout['header'] = array('default' => '',
-                              'label'   => __('Header Background','tcc-fluid'),
-                              'render'  => 'image',
-                              'divcss'  => 'upload-img',
-                              'media'   => array('title'  => __('Assign/Upload Header Background Image','tcc-fluid'),
-                                                 'button' => __('Assign Background', 'tcc-fluid'),
-                                                 'delete' => __('Remove Background', 'tcc-fluid')));//*/
-/*    $layout['back']   = array('label'   => __('Background','tcc-fluid'),
-                              'text'    => __('Use these options to add/change the background images','tcc-fluid'),
-                              'render'  => 'title');
-    $layout['header'] = array('label'   => __('Header',      'tcc-fluid'),
-                              'text'    => __('Coming Soon!','tcc-fluid'),
-                              'render'  => 'display');
-    $layout['footer'] = array('label'   => __('Footer',      'tcc-fluid'),
-                              'text'    => __('Coming Soon!','tcc-fluid'),
-                              'render'  => 'display');
-    $layout['site']   = array('label'   => __('Site',        'tcc-fluid'),
-                              'text'    => __('Coming Soon!','tcc-fluid'),
-                              'render'  => 'display'); //*/
-    $layout = apply_filters("tcc_{$this->base}_options_layout",$layout);
-    return $layout;
-  }
-
-  public function options_customize_register($wp_customize, TCC_Options_Fluidity $form) {
-    $wp_customize->add_section( 'fluid_'.$this->base, array('title' => $this->form_title(), 'priority' => $this->priority));
-    $form->customizer_settings($wp_customize,$this->base);
-  }
+	protected function options_layout() {
+		$layout = array(
+			'default' => true
+		);
+		$layout['paral'] = array(
+			'default' => 'no',
+			'label'   => __( 'Parallax', 'tcc-fluid' ),
+			'text'    => __( "Do you want to use the parallax effect for a page's featured image?", 'tcc-fluid' ),
+			'help'    => "No idea why this is called parallax, but it sounds very scientific.",
+			'render'  => 'radio',
+			'source'  => array(
+				'no'  => __( 'Show featured images normally - although currently this is not at all on pages', 'tcc-fluid' ),
+				'yes' => __( 'Use featured image as background', 'tcc-fluid' ),
+			)
+		);
+		$layout['title'] = array(
+			'default' => 'no',
+			'label'   => __( 'Page Title', 'tcc-fluid' ),
+			'text'    => __( 'Do you want to show the page title before the content?', 'tcc-fluid' ),
+			'render'  => 'radio',
+			'source'  => array(
+				'no'  => __( 'Do not show the page title.', 'tcc-fluid' ),
+				'yes' => __( 'Show the page title after the header and before the content.', 'tcc-fluid' ),
+			)
+		);
+		$layout['type'] = array(
+			'label'   => __( 'Typography', 'tcc-fluid' ),
+			'text'    => __( 'Site typography options', 'tcc-fluid' ),
+			'render'  => 'title',
+		);
+		$layout['font'] = array(
+			'default' => 'Helvitica Neue',
+			'label'   => __( 'Font Type', 'tcc-fluid' ),
+			'render'  => 'font',
+			'source'  => TCC_Options_Typography::mixed_fonts(),
+		);
+		$layout['size'] = array(
+			'default' => 18,
+			'label'   => __('Font Size','tcc-fluid'),
+			'stext'   => _x( 'px', "abbreviation for 'pixel' - not sure this even needs translating...", 'tcc-fluid' ),
+			'render'  => 'text',
+			'divcss'  => 'tcc_text_3em',
+		);
+		return apply_filters( "tcc_{$this->base}_options_layout", $layout );
+	}
 
 
 }
