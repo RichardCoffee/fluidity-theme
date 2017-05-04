602c0f99bba96c00271d02b51e85b44a02540723
Laptop 3 <richard.coffee@gmail.com>
2017-05-04 01:23:49 -0400

    testing

--- includes/wp-rest-api.php
+++ includes/wp-rest-api.php
@@ -8,9 +8,9 @@ if ( ! function_exists( 'fluidity_rest_authentication' ) ) {
 			return $result;
 		}
 		$status = tcc_option( 'status', 'apicontrol' );
-#		if ( ( $status === 'users' ) && ! is_user_logged_in() ) {
+		if ( ( $status === 'users' ) && ! is_user_logged_in() ) {
 			return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => rest_authorization_required_code() ) );
-#		}
+		}
 		return $result;
 	}
 	add_filter( 'rest_authentication_errors', 'fluidity_rest_authentication' );
