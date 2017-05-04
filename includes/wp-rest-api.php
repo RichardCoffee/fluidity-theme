e48c0434e1653b71ffbeeb6345f234ab164b1cfa
Laptop 3 <richard.coffee@gmail.com>
2017-05-04 01:25:38 -0400

    testing

--- includes/wp-rest-api.php
+++ includes/wp-rest-api.php
@@ -8,7 +8,8 @@ if ( ! function_exists( 'fluidity_rest_authentication' ) ) {
 			return $result;
 		}
 		$status = tcc_option( 'status', 'apicontrol' );
-		if ( ( $status === 'users' ) && ! is_user_logged_in() ) {
+#		if ( ( $status === 'users' ) && ! is_user_logged_in() ) {
+		if ( ( $status !== 'on' ) && ! is_user_logged_in() ) {
 			return new WP_Error( 'rest_not_logged_in', 'You are not currently logged in.', array( 'status' => rest_authorization_required_code() ) );
 		}
 		return $result;
