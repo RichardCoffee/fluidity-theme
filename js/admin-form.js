fae2a5336f832a20db20fd974c9a5a7ab7679970
Laptop 3 <richard.coffee@gmail.com>
2017-05-05 06:59:42 -0400

    testing - commented out new js code

--- js/admin-form.js
+++ js/admin-form.js
@@ -11,12 +11,12 @@ jQuery(document).ready(function() {
 	showhideAdminElements( '.privacy-plugin-active', '.privacy-plugin-filter',  'filter');
 	showhideAdminElements( '.privacy-theme-active',  '.privacy-theme-filter',   'filter');
 
-	if ( tcc_admin_options.showhide ) {
-		var count = tcc_admin_options.showhide.length;
-		for ( var i = 0; i < count; i++ ) {
-			targetableElements( tcc_admin_options.showhide[i] );
-		}
-	}
+//	if ( tcc_admin_options.showhide ) {
+//		var count = tcc_admin_options.showhide.length;
+//		for ( var i = 0; i < count; i++ ) {
+//			targetableElements( tcc_admin_options.showhide[i] );
+//		}
+//	}
 
 	showhideElements( jQuery( '.showhide' ) );
 	jQuery( '.form-colorpicker'  ).wpColorPicker();
