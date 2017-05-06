5d8a79f1a8db5b3eac77e8bdc5c541cea0e18151
Laptop 3 <richard.coffee@gmail.com>
2017-05-06 04:29:48 -0400

    remove div

--- template-parts/content-link.php
+++ template-parts/content-link.php
@@ -12,24 +12,20 @@ who_am_i(); ?>
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php microdata()->BlogPosting(); ?>>
 
 		<div><?php
-
+/*
 			$link_css = "col-lg-12 col-md-12 col-sm-12 col-xs-12";
 			if ( has_post_thumbnail() ) { // FIXME:  need option for image/title placement ?>
 				<div class="col-lg-3 col-md-3 hidden-sm hidden-xs">
 					<?php fluid_thumbnail(); ?>
 				</div><!-- .col-* --><?php
 				$link_css = "col-lg-9 col-md-9 col-sm-12 col-xs-12";
-			} ?>
+			} */ ?>
 
-			<div class="<?php echo $link_css; ?>">
+			<?php fluid_content_header(); ?>
 
-				<?php fluid_content_header(); ?>
-
-				<div class="article" itemprop="description">
-					<?php the_content(); ?>
-				</div><!-- .article -->
-
-			</div><!-- .col-* -->
+			<div class="article" itemprop="description">
+				<?php the_content(); ?>
+			</div><!-- .article -->
 
 		</div>
 
