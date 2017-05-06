6a77e8543210be7ad3ee311bb83f611399096230
Laptop 3 <richard.coffee@gmail.com>
2017-05-06 01:55:11 -0400

    testing algorithm change

--- classes/Theme/Pagination.php
+++ classes/Theme/Pagination.php
@@ -63,10 +63,10 @@ foreach( $stats as $stat ) {
 				for ( $i = 1; $i <= $this->pages; $i++ ) {
 					$lnorm = $this->paged - $this->range - 1;
 					$hnorm = $this->paged + $this->range + 1;
-$d1 = ( ( $i < $this->show ) && ( $i+1 < ( $this->pages - $this->show ) ) ) ? $this->range : 0;
+$d1 = ( ( $i < $this->paged ) && ( $i+1 < ( $this->pages - $this->show ) ) ) ? $this->range : 0;
 #$d1 = ( ( $i+1 < ( $this->pages - $this->show ) ) ) ? $this->range : 0;
 $d2 = ( ( $i > ( $this->pages - $this->show ) ) && ( $i < $this->show+1 ) ) ? $this->range : 0 ;
-$lrange = $this->paged - $this->range - $d1;
+$lrange = $this->paged - $this->range - 1 - $d1;
 $hrange = $this->paged + $this->range + 1 + $d2;
 echo "I:$i";
 echo "L:$lnorm/$lrange";
