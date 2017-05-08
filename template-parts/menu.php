693c1bba1fe1a4e722a0f47bcf1704a18d6ca842
Laptop 3 <richard.coffee@gmail.com>
2017-05-07 20:11:57 -0400

    incorporate apply_attrs method in menu template

--- template-parts/menu.php
+++ template-parts/menu.php
@@ -7,12 +7,16 @@
 
 $page = get_page_slug();
 $menu = apply_filters( 'tcc_menu', 'primary', $page );	#	defaults: primary, header or footer
-if (has_nav_menu($menu)) {
+if ( has_nav_menu( $menu ) ) {
 #	$main_css = get_menu_class($menu,$page);
-	if (tcc_layout('menu')==='bootstrap') {
+	if ( tcc_layout( 'menu' ) === 'bootstrap' ) {
 		/* bootstrap navigation */
-		$main_id  = "navbar-{$page}-$menu";
-		$main_css = "navbar navbar-fluidity navbar-$menu navbar-$page navbar-{$page}-$menu";
+		$nav_attrs = array(
+			'id'    => "navbar-{$page}-$menu",
+			'class' => "navbar navbar-fluidity navbar-$menu navbar-$page navbar-{$page}-$menu",
+			'role'  => 'navigation',
+		);
+		$nav_attrs = array_merge( $nav_attrs, microdata()->SiteNavigationElement( true ) );
 		$button_attrs = array(
 			'type'  => 'button',
 			'class' => 'navbar-toggle',
@@ -20,19 +24,19 @@ if (has_nav_menu($menu)) {
 			'aria-expanded' => 'false',
 			'data-toggle'   => 'collapse',
 			'data-target'   => ".navbar-$menu-collapse",
-		); ?>
-		<nav id="<?php echo $main_id; ?>" class="<?php echo $main_css; ?>" <?php microdata()->SiteNavigationElement(); ?> role="navigation">
+		);
+		fluid_library()->apply_attrs_tag( $nav_attrs, 'nav' ); ?>
 			<div class="navbar-header">
-				<button <?php fluid_library()->apply_attrs( $button_attrs ); ?>>
+				<?php fluid_library()->apply_attrs_tag( $button_attrs, 'button' ); ?>
 					<span class="sr-only">Toggle navigation</span>
 					<?php library()->fawe( 'fa-bars' ); ?>
 				</button>
-				<a class="navbar-brand " href="<?php echo esc_url( home_url() )?>"><?php bloginfo('name')?></a>
+				<a class="navbar-brand" href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a>
 			</div>
 			<div class="collapse navbar-collapse navbar-<?php echo $menu; ?>-collapse"><?php
 				// FIXME: add filter for wp_nav_menu($args)
 #				wp_nav_menu( array('menu'=>$menu,'container'=>false,'menu_class'=>'nav navbar-nav','walker'=> new TCC_NavWalker_Bootstrap(), 'fallback_cb' => '' ) );
-				require_once( FLUIDITY_HOME . 'vendor/wp-bootstrap-navwalker.php');
+				require_once( FLUIDITY_HOME . 'vendor/wp-bootstrap-navwalker.php' );
 				wp_nav_menu( array(
 					'menu'           => $menu,
 					'theme_location' => $menu,
@@ -46,18 +50,27 @@ if (has_nav_menu($menu)) {
 		</nav><?php
 	} else {
 		/*	underscore navigation */
-		$main_css = "main-navigation {$menu}-navigation {$page}-{$menu}-navigation";
+		$nav_attrs = array(
+			'id'    => 'site-navigation',
+			'class' => "main-navigation {$menu}-navigation {$page}-{$menu}-navigation",
+			'role'  => 'navigation',
+		);
+		$nav_attrs = array_merge( $nav_attrs, microdata()->SiteNavigationElement( true ) );
 		$button_attrs = array(
 			'class' => 'menu-toggle',
 			'aria-controls' => $menu,
 			'aria-expanded' => 'false',
-		); ?>
-		<nav id="site-navigation" class="<?php echo $main_css; ?>" <?php microdata()->SiteNavigationElement(); ?> role="navigation">
-			<button <?php fluid_library()->apply_attrs( $button_attrs ); ?>>
+		);
+		fluid_library()->apply_attrs_tag( $nav_attrs, 'nav' );
+			fluid_library()->apply_attrs_tag( $button_attrs, 'button' ); ?>
 				<span class="sr-only">Toggle navigation</span>
 				<?php library()->fawe( 'fa-bars' ); ?>
-			</button>
-			<?php wp_nav_menu( array( 'theme_location' => $menu, 'menu_id' => 'primary-menu', 'fallback_cb' => '' ) ); ?>
+			</button><?php
+			wp_nav_menu( array(
+				'theme_location' => $menu,
+				'menu_id' => 'primary-menu',
+				'fallback_cb' => '' )
+			); ?>
 		</nav><!-- #site-navigation --><?php
 	}
 }
