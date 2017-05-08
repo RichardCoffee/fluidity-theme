953d2d48aa9168c159e6c17b95117045e4ef949d
Laptop 3 <richard.coffee@gmail.com>
2017-05-08 02:19:37 -0400

    bugfix - actually load styles

--- functions.php
+++ functions.php
@@ -19,6 +19,7 @@ require_once(FLUIDITY_HOME.'includes/update.php');
 require_once('includes/bootstrap.php');
 require_once(FLUIDITY_HOME.'includes/options.php');  #  Needs full path, or wp-admin/includes/options.php gets loaded instead
 require_once('includes/colors.php');                 #  options.php must be loaded before colors.php
+require_once( 'includes/enqueue.php' );
 require_once('includes/library.php');
 require_once('includes/login.php');
 require_once('includes/menus.php');
