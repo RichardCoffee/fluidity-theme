6acb3526475046b5876d14e2922e7b758f663a3a
Laptop 3 <richard.coffee@gmail.com>
2018-03-06 11:19:27 -0500

    use Navigation class for all users

--- template-parts/content.php
+++ template-parts/content.php
@@ -22,11 +22,11 @@ who_am_i(); ?>
 
 	if ( is_single() ) {
 		$taxonomy = apply_filters( 'fluid_content_taxonomy', 'category' );
-		if ( current_user_can( 'manage_options' ) ) {
+#		if ( current_user_can( 'manage_options' ) ) {
 			new TCC_Theme_Navigation( array( 'taxonomy' => $taxonomy ) );
-		} else {
-			fluid_navigation( $taxonomy, true );
-		}
+#		} else {
+#			fluid_navigation( $taxonomy, true );
+#		}
 		echo "<p> </p>";
 		fluid_postmetadata();
 	}
