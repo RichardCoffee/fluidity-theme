280d5fe9afba489702de8aa9a90b04c20721644c
Laptop 3 <richard.coffee@gmail.com>
2018-03-13 04:59:20 -0400

    add check for is_bbpress

--- includes/third-party.php
+++ includes/third-party.php
@@ -8,40 +8,45 @@
 
 /**  bbPress  **/
 
-#  bugfix from http://www.rewweb.co.uk/bbpress-wp4-fix2/
-add_filter('bbp_show_lead_topic', '__return_true'); // FIXME:  has this bug been fixed yet?
+if ( function_exists( 'is_bbpress' ) && is_bbpress() ) {
 
-if ( ! function_exists( 'fluidity_bbpress_font_size' ) ) {
-	function fluidity_bbpress_font_size() {
-		$fontsize = tcc_design( 'bbpsize' );
-		if ( $fontsize && ( ! ( $fontsize === 12 ) ) ) {
-			$css = array(
-				'div#bbpress-forums',
-				'div#bbpress-forums div.bbp-breadcrumb',
-				'div#bbpress-forums ul.bbp-lead-topic',
-				'div#bbpress-forums ul.bbp-topics',
-				'div#bbpress-forums ul.bbp-forums',
-				'div#bbpress-forums ul.bbp-replies',
-				'div#bbpress-forums ul.bbp-search-results',
-			);
-			$css_tags = implode( ', ', $css );
-			echo "$css_tags {";
-			echo "	font-size:  {$fontsize}px;";
-			echo "}";
+	#  bugfix from http://www.rewweb.co.uk/bbpress-wp4-fix2/
+	add_filter('bbp_show_lead_topic', '__return_true'); // FIXME:  has this bug been fixed yet?
+
+	#  Change font sizes
+	if ( ! function_exists( 'fluidity_bbpress_font_size' ) ) {
+		function fluidity_bbpress_font_size() {
+			$fontsize = tcc_design( 'bbpsize' );
+			if ( $fontsize && ( ! ( $fontsize === 12 ) ) ) {
+				$css = array(
+					'div#bbpress-forums',
+					'div#bbpress-forums div.bbp-breadcrumb',
+					'div#bbpress-forums ul.bbp-lead-topic',
+					'div#bbpress-forums ul.bbp-topics',
+					'div#bbpress-forums ul.bbp-forums',
+					'div#bbpress-forums ul.bbp-replies',
+					'div#bbpress-forums ul.bbp-search-results',
+				);
+				$css_tags = implode( ', ', $css );
+				echo "$css_tags {";
+				echo "	font-size:  {$fontsize}px;";
+				echo "}";
+			}
+			$fontosize1 = tcc_design( 'bbposize1' );
+			if ( $fontosize1 && ( ! ( $fontosize1 === 11 ) ) ) {
+				$css1 = array(
+					'div#bbpress-forums .bbp-forum-info .bbp-forum-content',
+					'div#bbpress-forums p.bbp-topic-meta',
+				);
+				$css_tags1 = implode( ', ', $css1 );
+				echo "$css_tags1 {";
+				echo "	font-size:  {$fontosize1}px;";
+				echo "}";
+			}
 		}
-		$fontosize1 = tcc_design( 'bbposize1' );
-		if ( $fontosize1 && ( ! ( $fontosize1 === 11 ) ) ) {
-			$css = array(
-				'div#bbpress-forums .bbp-forum-info .bbp-forum-content',
-				'div#bbpress-forums p.bbp-topic-meta',
-			);
-			$css_tags = implode( ', ', $css );
-			echo "$css_tags {";
-			echo "	font-size:  {$fontosize1}px;";
-			echo "}";
-		}
+		add_action( 'tcc_custom_css', 'fluidity_bbpress_font_size' );
 	}
-	add_action( 'tcc_custom_css', 'fluidity_bbpress_font_size' );
+
 }
 
 
