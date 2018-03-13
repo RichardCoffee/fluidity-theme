7ad9bff49b38ebbd3141ca72d30345fd5219716d
Laptop 3 <richard.coffee@gmail.com>
2018-03-13 09:28:50 -0400

    log_entry

--- functions.php
+++ functions.php
@@ -98,6 +98,7 @@ add_filter('tcc_template-parts_root', function( $rootslug, $pageslug ) {
 	return $rootslug;
 }, 10, 2);
 add_filter('tcc_theme_sidebar_args', function($args) {
+log_entry($args);
 	if ( $args['sidebar'] === 'forum' ) {
 		$args['position'] = 'none';
 	}
