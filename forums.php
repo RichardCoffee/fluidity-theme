60edd814425a15827599b3f3d6429c8aa6d3952f
Laptop 3 <richard.coffee@gmail.com>
2018-03-13 01:07:27 -0400

    add forums.php

---
+++ forums.php
@@ -0,0 +1,75 @@
+<?php
+/*
+ *  File Name: stock.php
+ *
+ */
+
+get_header();
+
+$mypage = get_page_slug();
+
+#add_filter ( "fluid_{$mypage}_container_type", function ( $css ) { return 'container'; } );
+#add_filter ( "fluid_sidebar_css_$mypage",      function ( $css, $mypage ) { return 'col-lg-3 col-md-3 col-sm-12 col-xs-12';  }, 10, 2 );
+#add_filter ( "tcc_main_tag_css_$mypage",       function ( $css ) { return "col-lg-9 col-md-9 col-sm-12 col-xs-12"; } );
+
+TCC_Theme_Sidebar::get_instance( array() );	#	See docs/sidebar.txt on what values the array can have
+log_entry( "Page:  $mypage" );
+do_action( "tcc_top_$mypage", $mypage ); ?>
+
+<div id="fluid-content" class="fluid-<?php e_esc_attr( $mypage); ?> <?php e_esc_attr( container_type( $mypage ) ); ?>" <?php microdata()->Blog(); ?>>
+
+	<?php do_action( 'tcc_page_top', $mypage ); ?>
+
+	<div class="row">
+		<?php who_am_i(); ?>
+
+		<?php do_action( 'tcc_before_main', $mypage ); ?>
+
+		<main id="content" class="<?php echo tcc_main_tag_css( '' ); ?>" role="main" tabindex="-1"><?php
+
+			do_action( 'tcc_before_posts', $mypage );
+			do_action( "tcc_before_posts_$mypage" );
+
+			if ( have_posts() ) {
+
+				do_action( "tcc_before_loop_$mypage" );
+
+				$root = ( is_single() || is_page() || is_singular() ) ? 'content' : tcc_option( 'content' );
+				$root = apply_filters( 'tcc_template-parts_root', $root, $mypage );
+				while ( have_posts () ) { ?>
+					<div><?php
+						the_post();
+						$stem = fluid_content_slug( $mypage );
+						get_template_part( "template-parts/$root", $stem );
+						if ( ! is_singular() ) {
+							fluid_post_separator( $mypage );
+						} ?>
+					</div><?php
+				}
+
+				if ( ! is_singular() ) { ?>
+					<div class="row">
+						<div class="text-wide text-center">
+							<?php new TCC_Theme_Pagination(); ?>
+						</div>
+					</div><?php
+				}
+
+#				do_action( "tcc_after_loop_$mypage" );
+			} else {
+#				do_action( "tcc_no_loop_$mypage" );
+			}
+
+#			do_action( "tcc_after_posts_$mypage" );
+			do_action( 'tcc_after_posts' ); ?>
+
+		</main><!-- #content -->
+
+		<?php do_action( 'tcc_after_main' ); ?>
+
+	</div>
+</div><!-- #fluid-content --><?php
+
+#do_action( "tcc_bottom_$mypage" );
+
+get_footer();
