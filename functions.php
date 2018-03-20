16f2b4bdb8ec8ace29a32ca808e4edc9842d63f9
Laptop 3 <richard.coffee@gmail.com>
2018-03-20 14:33:27 -0400

    fix includes path

--- functions.php
+++ functions.php
@@ -15,7 +15,7 @@ defined( 'TCC_THEME_VERSION' ) or define( 'TCC_THEME_VERSION', FLUIDITY_VERSION 
 
 require_once('includes/loader.php');
 require_once('includes/debugging.php');              #  load logging function as soon as possible
-require_once(FLUIDITY_HOME.'includes/update.php');
+require_once(FLUIDITY_HOME.'includes/update.php');   #  Needs full path, or wp-admin/includes/update.php gets loaded instead
 require_once('includes/bootstrap.php');
 require_once(FLUIDITY_HOME.'includes/options.php');  #  Needs full path, or wp-admin/includes/options.php gets loaded instead
 require_once('includes/colors.php');                 #  options.php must be loaded before colors.php
@@ -23,7 +23,7 @@ require_once( 'includes/enqueue.php' );
 require_once('includes/library.php');
 require_once('includes/login.php');
 require_once('includes/menus.php');
-require_once(FLUIDITY_HOME.'includes/misc.php');
+require_once(FLUIDITY_HOME.'includes/misc.php');     #  Needs full path, or wp-admin/includes/misc.php gets loaded instead
 require_once('includes/parallax.php');
 require_once('includes/sidebars.php');
 require_once(FLUIDITY_HOME.'includes/widgets.php');  #  Needs full path, or wp-admin/includes/widgets.php gets loaded instead
@@ -38,8 +38,8 @@ fluid_login(); # creates instance of TCC_Theme_Login class
 
 if ( is_admin() ) {
 	fluid_options();
-	require_once( 'includes/admin.php' );
-	require_once(FLUIDITY_HOME.'includes/dashboard.php');
+	require_once( FLUIDITY_HOME . 'includes/admin.php' );      #  Needs full path, or wp-admin/includes/admin.php gets loaded instead
+	require_once( FLUIDITY_HOME . 'includes/dashboard.php' );  #  Needs full path, or wp-admin/includes/dashboard.php gets loaded instead
 } else {
 	require_once('includes/comments.php');
 	require_once('includes/footer.php');
