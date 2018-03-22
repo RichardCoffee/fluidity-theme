0e039c8f4a280b036ba3d5b2917bd885b632b1f4
Laptop 3 <richard.coffee@gmail.com>
2018-03-22 01:12:49 -0400

    add div around excerpt title text

--- template-parts/excerpt.php
+++ template-parts/excerpt.php
@@ -9,19 +9,23 @@ who_am_i(); ?>
 
 <div class="<?php clearfix()->div_class(); ?>">
 
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php microdata()->BlogPosting(); ?>><?php
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php microdata()->BlogPosting(); ?>>
 
-		$format  = esc_html__( 'Permanent Link to %s', 'tcc-fluid' );
-		$tooltip = sprintf( $format, get_the_title() ); ?>
-		<h2 class="text-center" itemprop="headline">
-			<?php tcc_post_title( 40 ); ?>
-		</h2>
+		<div class="excerpt-title-wrap"><?php
 
-		<?php if ( tcc_option( 'exdate' ) === 'show' ) { ?>
-			<h3 class="text-center">
-				<?php fluid_post_date(); ?>
-			</h3><?php
-		} ?>
+			$format  = esc_html__( 'Permanent Link to %s', 'tcc-fluid' );
+			$tooltip = sprintf( $format, get_the_title() ); ?>
+			<h2 class="text-center" itemprop="headline">
+				<?php tcc_post_title( 40 ); ?>
+			</h2>
+
+			<?php if ( tcc_option( 'exdate' ) === 'show' ) { ?>
+				<h3 class="text-center">
+					<?php fluid_post_date(); ?>
+				</h3><?php
+			} ?>
+
+		</div>
 
 		<div class="article" itemprop="description"><?php
 			the_excerpt(); ?>
