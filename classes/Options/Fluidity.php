cfc1b7d07717b1515c069857df171dec24aff9c2
Laptop 3 <richard.coffee@gmail.com>
2018-03-25 09:46:47 -0400

    retest original

--- classes/Options/Fluidity.php
+++ classes/Options/Fluidity.php
@@ -23,6 +23,7 @@ class TCC_Options_Fluidity extends TCC_Form_Admin {
 		add_action( 'tcc_admin_menu_setup',     array( $this, 'initialize_options' ) );
 		add_filter( 'form_text_' . $this->slug, array( $this, 'form_trans_text' ), 10, 2 );
 		parent::__construct();
+$this->add_currency_symbol();
 		# https://codex.wordpress.org/Plugin_API/Admin_Screen_Reference
 		add_action( 'current_screen', function() {
 			$this->check_screen( 'options-general', 'add_currency_symbol' );
