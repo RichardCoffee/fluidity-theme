bfbf50d4a9361d32b8a66bb0b2690889b045d39a
Laptop 3 <richard.coffee@gmail.com>
2018-04-14 18:29:16 -0400

    base test

--- classes/Widget/Login.php
+++ classes/Widget/Login.php
@@ -10,11 +10,11 @@ fluid()->log(
 	'is admin:  ' . is_admin(),
 	' is ajax:  ' . is_ajax()
 );
-		if ( is_user_logged_in() ) {
-			if ( ! is_admin() || ( is_admin() && is_ajax() ) ) {
+#		if ( is_user_logged_in() ) {
+#			if ( ! is_admin() || ( is_admin() && is_ajax() ) ) {
 				$this->title = esc_html__( 'Logout', 'tcc-fluid' );
-			}
-		}
+#			}
+#		}
 
 		$this->desc  = esc_html__( 'Fluidity Login form', 'tcc-fluid' );
 		$this->slug  = 'tcc_login';
