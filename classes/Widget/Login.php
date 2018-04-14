f008e2a02a01d56d659f46ca242e74e7a28feed4
Laptop 3 <richard.coffee@gmail.com>
2018-04-14 19:08:13 -0400

    test changing title

--- classes/Widget/Login.php
+++ classes/Widget/Login.php
@@ -2,10 +2,12 @@
 
 class TCC_Widget_Login extends TCC_Widget_Widget {
 
+	protected $id_base = 'fluid_login';
+
 	public function __construct() {
 		$this->title = esc_html__( 'Login', 'tcc-fluid' );
 		$this->desc  = esc_html__( 'Fluidity Login form', 'tcc-fluid' );
-		$this->slug  = 'tcc_login';
+		$this->slug  = $this->id_base; // TODO: DEPRECATE
 		parent::__construct();
 		add_filter( 'widget_display_callback', array( $this, 'fluid_login_title' ), 10, 3 );
 	}
@@ -16,14 +18,14 @@ class TCC_Widget_Login extends TCC_Widget_Widget {
 	}
 
 	public function fluid_login_title( $instance, $widget, $args ) {
+		if ( ( $this->id_base === $widget->id_base ) && is_user_logged_in() ) {
 fluid()->log(
 	$instance,
 	$widget,
 	$args
 );
-#		if ( ( $this->slug === $id ) && is_user_logged_in() ) {
-#			$instance['title'] = esc_html__( 'Logout', 'tcc-fluid' );
-#		}
+			$instance['title'] = esc_html__( 'Logout', 'tcc-fluid' );
+		}
 		return $instance;
 	}
 
