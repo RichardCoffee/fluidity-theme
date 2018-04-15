8c01b3ca278f83cad8a1ee72c61fbde30142a4e2
Laptop 3 <richard.coffee@gmail.com>
2018-04-15 17:47:11 -0400

    stop logging

--- classes/Options/Customizer.php
+++ classes/Options/Customizer.php
@@ -19,18 +19,14 @@ class TCC_Options_Customizer {
 		foreach( $controls as $section ) {
 			$priority = 0;
 			$section_id = $section['id'];
- $temp = $this->customizer_section( $section['section'] );
-fluid()->log($temp);
- 			$customize->add_section( $section['id'], $temp );
-#			$customize->add_section( $section['id'], $this->customizer_section( $section['section'] ) );
+			$section['section'] = $this->customizer_section( $section['section'] );
+			$customize->add_section( $section['id'], $section['section'] );
 			$controls = apply_filters( "fluid_customizer_controls_{$section['id']}", $section['controls'] );
-			foreach( $section['controls'] as $key => $control ) {
+			foreach( $controls as $key => $control ) {
 				$priority  += 10;
 				$setting_id = $section_id . '_' . $key;
-$temp=$this->customizer_control( $control );
-fluid()->log($temp);
-				$customize->add_setting( $setting_id, $temp );
-#				$customize->add_setting( $setting_id, $this->customizer_control( $control ) );
+				$control    = $this->customizer_control( $control );
+				$customize->add_setting( $setting_id, $control );
 				new TCC_Form_Customizer( compact( 'customize', 'section_id', 'setting_id', 'control', 'priority' ) );
 			}
 		}
