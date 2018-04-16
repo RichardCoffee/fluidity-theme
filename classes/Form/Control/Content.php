dd7032039a525dacdc4f08bf216929bd9306607c
Laptop 3 <richard.coffee@gmail.com>
2018-04-16 16:12:24 -0400

    logging

--- classes/Form/Control/Content.php
+++ classes/Form/Control/Content.php
@@ -27,7 +27,7 @@ class TCC_Form_Control_Content extends WP_Customize_Control {
 	 * @return  void
 	 */
 	public function render_content() {
-
+fluid()->log($this);
 		switch ( $this->type ) {
 
 			case 'content' :
