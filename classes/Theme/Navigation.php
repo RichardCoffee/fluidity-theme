c64a7ee9b420bcb5a47efe4eab8b7313639c7266
Laptop 3 <richard.coffee@gmail.com>
2018-04-17 04:32:10 -0400

    fix method call

--- classes/Theme/Navigation.php
+++ classes/Theme/Navigation.php
@@ -35,6 +35,7 @@ class TCC_Theme_Navigation extends TCC_Theme_BasicNav {
 		$this->taxonomy = apply_filters( 'fluid_navigation_taxonomy', $this->taxonomy );
 		$this->check_posts();
 		$this->navigation();
+$this->log($this);
 	}
 
 	protected function navigation_text() {
