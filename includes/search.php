427e3e15588ee10ccd64d3c1b7117319305b30c1
Laptop 3 <richard.coffee@gmail.com>
2018-04-21 03:49:31 -0400

    revert div

--- includes/search.php
+++ includes/search.php
@@ -16,10 +16,8 @@ if ( ! function_exists( 'fluid_show_search_form' ) ) {
 				fluid()->apply_attrs_element( 'label', $form['label'], $form['text']['label'] );
 				if ( isset( $form['hidden'] ) ) {
 					fluid()->apply_attrs_element( 'input', $form['hidden'] );
-				} ?>
-				<div>
-					<?php fluid()->apply_attrs_element( 'input', $form['search'] ); ?>
-				</div>
+				}
+				fluid()->apply_attrs_element( 'input', $form['search'] ); ?>
 				<span class="input-group-btn">
 					<button <?php fluid()->apply_attrs( $form['button'] ); ?>>
 						<?php fluid()->fawe( 'fa-search' ); ?>
