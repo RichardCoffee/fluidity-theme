b79e95bf91d6036216e344dba47a62a5d93d2dd9
Laptop 3 <richard.coffee@gmail.com>
2018-04-22 14:03:21 -0400

    remove span from search form

--- includes/search.php
+++ includes/search.php
@@ -13,20 +13,18 @@ if ( ! function_exists( 'fluid_show_search_form' ) ) {
 		$form = apply_filters( 'fluid_show_search_form', $form ); ?>
 		<form <?php fluid()->apply_attrs( $form['form'] ); ?>>
 			<div class="input-group"><?php
-				fluid()->apply_attrs_element( 'label', $form['label'], $form['text']['label'] ); ?>
-				<span class="block"><?php
-					if ( isset( $form['hidden'] ) ) {
-						fluid()->apply_attrs_element( 'input', $form['hidden'] );
-					}
-					fluid()->apply_attrs_element( 'input', $form['search'] ); ?>
-					<span class="input-group-btn">
-						<button <?php fluid()->apply_attrs( $form['button'] ); ?>>
-							<?php fluid()->fawe( 'fa-search' ); ?>
-							<span class="screen-reader-text">&nbsp;
-								<?php esc_html( $form['text']['button'] ); ?>
-							</span>
-						</button>
-					</span>
+				fluid()->apply_attrs_element( 'label', $form['label'], $form['text']['label'] );
+				if ( isset( $form['hidden'] ) ) {
+					fluid()->apply_attrs_element( 'input', $form['hidden'] );
+				}
+				fluid()->apply_attrs_element( 'input', $form['search'] ); ?>
+				<span class="input-group-btn">
+					<button <?php fluid()->apply_attrs( $form['button'] ); ?>>
+						<?php fluid()->fawe( 'fa-search' ); ?>
+						<span class="screen-reader-text">&nbsp;
+							<?php esc_html( $form['text']['button'] ); ?>
+						</span>
+					</button>
 				</span>
 			</div>
 		</form><?php
