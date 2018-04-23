778a7383dad32a28cea90b1fc82b6d615b9c06a2
Laptop 3 <richard.coffee@gmail.com>
2018-04-23 05:11:32 -0400

    chrome to meta

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html <?php language_attributes(); ?> <?php microdata()->WebPage(); ?>>
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<?php do_action( 'fluid_header_links' ); ?>
