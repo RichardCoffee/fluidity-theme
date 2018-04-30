bbcfc6346124bd9081f9065a6c2d384a94547177
Laptop 3 <richard.coffee@gmail.com>
2018-04-30 18:43:30 -0400

    cleanup the code a little bit

--- js/customizer.js
+++ js/customizer.js
@@ -1,4 +1,4 @@
-// https://florianbrinkmann.com/en/3783/conditional-displaying-and-hiding-of-customizer-controls-via-javascript/
+
 ;(function () {
 
 	var api = wp.customize;
@@ -9,21 +9,30 @@
 	api.bind('ready', function () {
 
 		for ( var target in fluid_customize ) {
-			console.log(target);
 			// wp-content/themes/twentyseventeen/assets/js/customize-controls.js
 			api( fluid_customize[ target ].control, function( setting ) {
-				console.log(fluid_customize [ target ].control);
 				api.control( target, function( control ) {
-					console.log( "control: ", control );
 					var visibility = function() {
-						var index = fluid_customize[ control.id ].control;
-						console.log('id: '+control.id,'index: '+index);
 						if ( fluid_customize[ control.id ].action === 'hide' ) {
-							console.log('check: '+fluid_customize[ control.id ].setting,'setting: '+setting.get());
 							if ( fluid_customize[ control.id ].setting === setting.get() ) {
 								control.container.slideUp( 180 );
+								//       or
+								//control.deactivate();
+								//       or
+								//control.active.set( false );
+								//control.active.validate = function() { return false; };
 							} else {
 								control.container.slideDown( 180 );
+								//control.activate();
+							}
+						}
+						if ( fluid_customize[ control.id ].action === 'show' ) {
+							if ( fluid_customize[ control.id ].setting === setting.get() ) {
+								control.container.slideDown( 180 );
+								//control.activate();
+							} else {
+								control.container.slideUp( 180 );
+								//control.deactivate();
 							}
 						}
 					};
@@ -33,53 +42,18 @@
 			} );
 		}
 
+		/***   handle postMessage tasks   ***/
 
-	/***   handle postMessage tasks   ***/
-
-	// Site title.
-	api( 'blogname', function( value ) {
-		value.bind( function( to ) {
-			jQuery( 'a.navbar-brand' ).text( to );
+		// Site title.
+		api( 'blogname', function( value ) {
+			value.bind( function( to ) {
+				jQuery( 'a.navbar-brand' ).text( to );
+			} );
 		} );
+
 	} );
+} )();
 
-
-/*
-		wp.customize.control('sidebar_position', function (control) {
-			/**
-			 * Run function on setting change of control.
-			 * /
-			control.setting.bind(function (value) {
-				switch (value) {
-					/**
-					 * The select was switched to the hide option.
-					 * /
-					case 'none':
-						/**
-						 * Deactivate the conditional control.
-						 * /
-						wp.customize.control('sidebar_fluidity').deactivate();
-wp.customize.control('sidebar_fluidity').active.set( false );
-wp.customize.control('sidebar_fluidity').active.validate = function() {
-	return false; // Prevent preview from updating state.
-};
-						break;
-					/**
-					 * The select was switched to »show«.
-					 * /
-//					case 'show':
-					default:
-						/**
-						 * Activate the conditional control.
-						 * /
-						wp.customize.control('sidebar_fluidity').activate();
-						break;
-				}
-			});
-		});
-*/
-	});
-})();
 /*
 // http://justintadlock.com/archives/2015/05/26/multiple-checkbox-customizer-control
 jQuery( document ).ready( function() {
