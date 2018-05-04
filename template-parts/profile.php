b6dc83dd06c3fb3049442d25b7c4e8735bfc67db
Laptop 3 <richard.coffee@gmail.com>
2018-05-04 16:18:56 -0400

    remove css classes

--- template-parts/profile.php
+++ template-parts/profile.php
@@ -6,8 +6,8 @@
 
 defined( 'ABSPATH' ) || exit; ?>
 
-<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12" <?php microdata()->Person(); ?>>
-	<h1>
+<div <?php microdata()->Person(); ?>>
+	<h1 >
 		<?php printf( esc_html_x( 'All posts by %s', "post author's name", 'tcc-fluid' ), get_the_author() ); ?>
 	</h1><?php
 	if ( $descrip = get_the_author_meta( 'description' ) ) { ?>
