f5cc49741ef4daeea237d850e208824f93a2c505
Laptop 3 <richard.coffee@gmail.com>
2018-05-22 03:43:40 -0400

    add property and spacer

--- classes/Form/Field/CheckBox.php
+++ classes/Form/Field/CheckBox.php
@@ -4,13 +4,14 @@ class TCC_Form_Field_CheckBox extends TCC_Form_Field_Field {
 
 	protected $type    = 'checkbox';
 	protected $checked = false;
+	protected $form_control = false;
 
 	public function checkbox() {
 		echo $this->get_checkbox();
 	}
 
 	public function get_checkbox() {
-		return $this->get_tag( 'label', [ 'id' => $this->field_id . '_label' ] ) . $this->get_input() . $this->description . '</label>';
+		return $this->get_tag( 'label', [ 'id' => $this->field_id . '_label' ] ) . $this->get_input() . '&nbsp;' . $this->description . '</label>';
 	}
 
 	protected function get_input_attributes() {
