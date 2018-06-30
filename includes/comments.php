d5f1709503dedb69ef902eb77fb38804c70624a0
Laptop 3 <richard.coffee@gmail.com>
2018-06-30 17:43:30 -0400

    fix - remove spurious closing php tag

--- includes/comments.php
+++ includes/comments.php
@@ -107,7 +107,7 @@ if (!function_exists('fluid_list_comments')) {
 						comment_reply_link( [ 'depth' => $depth, 'max_depth' => $args['max_depth'] ], $comment->comment_ID, $comment->comment_post_ID ); ?>
 					<div><!-- .reply -->
 					<br /><?php
-				} ?>
+				}
 		} ?>
 		<hr class="comment-separator"><?php
 	}
