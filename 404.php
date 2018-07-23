da0ee8ffbcb58cd33712b0070b9324c21f4b51ee
Laptop 3 <richard.coffee@gmail.com>
2018-07-23 01:35:58 -0400

    refactor code on the 404 page

--- 404.php
+++ 404.php
@@ -29,34 +29,16 @@ if (WP_DEBUG && is_user_logged_in() && current_user_can('update_core')) { ?>
 <div class="<?php container_type( $mypage ); ?>">
 	<div class="row text-center">
 		<div class="col-md-12">
-			<article class="error-404 not-found">
-
-				<h2  class="page-title">
-					<?php esc_html_e( "Ooops.....Well this is somewhat embarrassing, isn't it?", 'tcc-fluid' ); ?>
-				</h2>
-
-				<div class="page-content">
-
-					<h3>
-						<?php esc_html_e( 'It seems as if the page you are looking for is not here', 'tcc-fluid' ); ?>
-					</h3>
-
-					<h4>
-						<?php esc_html_e( 'It looks like nothing was found at this location', 'tcc-fluid' ); ?>
-					</h4
->
+			<article class="error-404 not-found"><?php
+				fluid()->element( 'h2', [ 'class' => 'page-title' ], __( "Ooops.....Well this is somewhat embarrassing, isn't it?", 'tcc-fluid' ) ); ?>
+				<div class="page-content"><?php
+					fluid()->element( 'h3', [ ], __( 'It seems as if the page you are looking for is not here', 'tcc-fluid' ) );
+					fluid()->element( 'h4', [ ], __( 'It looks like nothing was found at this location', 'tcc-fluid' ) ); ?>
 					<div class="row">
 						<div class="col-lg-4 col-lg-offset-4 col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2 col-xs-12">
 							<?php get_search_form(); ?>
 						</div>
 					</div>
-
-					<h1>
-						<a href="<?php echo esc_html( home_url() ); ?>">
-							<?php esc_html_e( 'Home Page', 'tcc-fluid' ); ?>
-						</a>
-					</h1>
-
 				</div><!-- .page-content -->
 			</article>
 		</div><!-- .col-md-12 -->
