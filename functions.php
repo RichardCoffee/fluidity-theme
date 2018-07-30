0537e29070f5cfdce73c2cd7c9f82787895b7065
Laptop 3 <richard.coffee@gmail.com>
2018-07-30 03:40:47 -0400

    log

--- functions.php
+++ functions.php
@@ -85,3 +85,4 @@ add_filter( 'heartbeat_received', function ( $resource, $data ) {
 
 #fluid()->log( fluid()->get_html_attributes( '<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">' ) );
 #fluid()->log( wp_kses_allowed_html( 'data' ) );
+fluid()->log( get_theme_mods() );
