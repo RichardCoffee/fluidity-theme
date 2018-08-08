73c7b3554c6818994d5d9ce58d6fc72a2a404379
Laptop 3 <richard.coffee@gmail.com>
2018-08-07 20:23:37 -0400

    Chrome Frame no longer supported

--- header.php
+++ header.php
@@ -3,7 +3,7 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<?php do_action( 'fluid_header_links' ); ?>
