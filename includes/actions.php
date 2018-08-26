17d492b06618d542775f90ecbb1fe38eaf170109
Laptop 3 <richard.coffee@gmail.com>
2018-08-26 13:43:48 -0400

    add function fluid_page_top

--- includes/actions.php
+++ includes/actions.php
@@ -1,9 +1,19 @@
 <?php
 
+if ( ! function_exists( 'fluid_page_top' ) ) {
+	function fluid_page_top( $page_slug ) {
+		if ( has_action( "fluid_page_top_$page_slug" ) ) {
+			do_action( "fluid_page_top_$page_slug" );
+		}
+	}
+}
+
 if ( ! function_exists( 'fluid_content_header' ) ) {
 	function fluid_content_header() {
-		$page = get_page_slug();
-		do_action( "fluid_content_header_$page" );
+		$page_slug = get_page_slug();
+		if ( has_action( "fluid_content_header_$page_slug" ) ) {
+			do_action( "fluid_content_header_$page_slug" );
+		}
 	}
 	add_action( 'fluid_content_header', 'fluid_content_header' );
 }
