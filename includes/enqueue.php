6a1d54adde7b84021726d3f5e2fabe3c12b0bd8e
Laptop 3 <richard.coffee@gmail.com>
2018-09-01 19:02:15 -0400

    push update remove emoji actions

--- includes/enqueue.php
+++ includes/enqueue.php
@@ -104,9 +104,9 @@ if ( ! function_exists( 'fluid_dequeue_emoji' ) ) {
 	/**
 	 * Remove the emoji styles.
 	 */
-	remove_action( 'wp_print_styles', 'print_emoji_styles' );
-	remove_action( 'wp_print_scripts', 'print_emoji_detection_script' );
-	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+	remove_action( 'wp_print_styles',    'print_emoji_styles' );
 	remove_action( 'admin_print_styles', 'print_emoji_styles');
+	remove_action( 'wp_head',            'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts','print_emoji_detection_script');
+	remove_action( 'embed_head',         'print_emoji_detection_script' );
 }
