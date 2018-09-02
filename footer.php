6fda6a5a0d5a07206a29f043dbac33351f3d47e0
Laptop 3 <richard.coffee@gmail.com>
2018-09-02 12:58:27 -0400

    actually send rewrite

--- footer.php
+++ footer.php
@@ -10,9 +10,9 @@ defined( 'ABSPATH' ) || exit; ?>
 		<footer>
 
 			<div id="fluid-footer" class="<?php e_esc_attr( container_type( 'footer' ) ); ?>" <?php microdata()->WPFooter(); ?>><?php
-				$slug = get_page_slug();
 				$dir  = apply_filters( 'fluid_footer_template_dir', 'template-parts', $slug );
 				$root = apply_filters( 'fluid_footer_template_root', 'footer', $slug );
+				$slug = get_page_slug();
 				get_template_part( "$dir/$root", $slug ); ?>
 			</div>
 
