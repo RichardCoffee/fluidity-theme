659662e210d483c5b6a7cc3f04639a32c68d042f
Laptop 3 <richard.coffee@gmail.com>
2018-09-02 16:17:16 -0400

    add method telephone_format

--- classes/microdata.php
+++ classes/microdata.php
@@ -367,6 +367,13 @@ class TCC_Microdata {
 		return '<span itemprop="telephone">' . esc_html( $phone ). '</span>';
 	}
 
+	public function telephone_format( $phone ) {
+		if ( ! ( strpos( $phone, 'itemprop' ) === false ) ) { return $phone; }
+		$cleaned = preg_replace( "/[^0-9]/", "", $phone );
+		$string  = '<a href="telephone:%s" itemprop="telephone">%s</a>';
+		return sprintf( $string, $cleaned, $phone );
+	}
+
 	public function url_format( $url ) {
 		if ( ! ( strpos( $url, 'itemprop' ) === false ) ) { return $url; }
 		$string = '<a href="%s" itemprop="url">%s</a>';
