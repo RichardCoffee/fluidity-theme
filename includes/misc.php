7ffd9e7358813711d2d59541c94dcb6616c177a0
Laptop 3 <richard.coffee@gmail.com>
2018-09-01 21:43:37 -0400

    add debugging check

--- includes/misc.php
+++ includes/misc.php
@@ -220,7 +220,9 @@ if ( ! function_exists( 'fluid_php_error_alerts' ) ) {
 			die();
 		}
 	}
-	set_error_handler( 'fluid_php_error_alerts', E_ERROR ^ E_CORE_ERROR ^ E_COMPILE_ERROR ^ E_USER_ERROR ^ E_RECOVERABLE_ERROR ^ E_WARNING ^ E_CORE_WARNING ^ E_COMPILE_WARNING ^ E_USER_WARNING ^ E_NOTICE ^ E_USER_NOTICE ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_PARSE );
+	if ( WP_DEBUG ) {
+		set_error_handler( 'fluid_php_error_alerts', E_ERROR ^ E_CORE_ERROR ^ E_COMPILE_ERROR ^ E_USER_ERROR ^ E_RECOVERABLE_ERROR ^ E_WARNING ^ E_CORE_WARNING ^ E_COMPILE_WARNING ^ E_USER_WARNING ^ E_NOTICE ^ E_USER_NOTICE ^ E_DEPRECATED ^ E_USER_DEPRECATED ^ E_PARSE );
+	}
 }
 
 /**
