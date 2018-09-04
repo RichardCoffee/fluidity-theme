a25818470de6966bbe6d6f28f80a40902dc16076
Laptop 3 <richard.coffee@gmail.com>
2018-09-04 13:43:37 -0400

    add file template-parts/content-attachment.php

---
+++ template-parts/content-attachment.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ *  File Name: template-parts/content.php
+ *
+ *  Notes:  can only be used within The Loop
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+who_am_i(); ?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> <?php microdata()->BlogPosting(); ?> role="article">
+
+	<header><?php
+		do_action( 'fluid_content_header' ); ?>
+	</header>
+
+	<section class="article" itemprop="articleBody"><?php
+		the_attachment_link( 0, 'full' );
+#		the_content(); ?>
+	</section>
+
+	<footer><?php
+		do_action( 'fluid_content_footer' ); ?>
+	</footer>
+
+</article>
