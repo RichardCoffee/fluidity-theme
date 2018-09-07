46984a4c8c25b38f224f6c119dfa0f9c745f3200
Laptop 3 <richard.coffee@gmail.com>
2018-09-07 14:45:20 -0400

    add add_action call

--- includes/actions.php
+++ includes/actions.php
@@ -1,11 +1,14 @@
 <?php
 
+defined( 'ABSPATH' ) || exit;
+
 if ( ! function_exists( 'fluid_page_top' ) ) {
 	function fluid_page_top( $page_slug ) {
 		if ( has_action( "fluid_page_top_$page_slug" ) ) {
 			do_action( "fluid_page_top_$page_slug" );
 		}
 	}
+	add_action( 'fluid_page_top', 'fluid_page_top' );
 }
 
 if ( ! function_exists( 'fluid_content_header' ) ) {
