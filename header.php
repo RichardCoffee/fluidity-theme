5859012a21a30ee8e77ee1b64d796d78040d4b82
Richard Coffee <richard.coffee@gmail.com>
2019-06-19 04:51:59 -0400

    implement wp_body_open call

--- header.php
+++ header.php
@@ -31,6 +31,9 @@ defined( 'ABSPATH' ) || exit; ?>
 
 <body <?php body_class( ); ?> <?php fluid_schema_page_check(); ?>>
 	<a class="skip-link sr-only" href="#fluid-content"><?php esc_html_e( 'Skip to content', 'tcc-fluid' ); ?></a><?php
+	if ( function_exists( 'wp_body_open' ) ) {
+		wp_body_open();
+	}
 	$slug = get_page_slug();
 	$dir  = apply_filters( 'fluid_header_template_dir', 'template-parts', $slug );
 	$root = apply_filters( 'fluid_header_template_root', 'header', $slug );
